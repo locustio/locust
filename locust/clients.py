67ea3389333fc4141ec775975584b28e07eaa017
Jonatan Heyman <jonatan@heyman.info>
2011-02-07 18:02:53 +0100

    Added docstrings to clients.py. Improved docstrings in core.py

--- locust/clients.py
+++ locust/clients.py
@@ -12,21 +12,48 @@ class HTTPClient(object):
 
 
 class HttpBrowser(object):
+    """
+    Class for performing web requests and holding session cookie between requests (in order
+    to be able to log in to websites). 
+    
+    Logs each request so that locust can display statistics.
+    """
+    
     def __init__(self, base_url):
         self.base_url = base_url
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
         urllib2.install_opener(self.opener)
     
     @log_request
-    def get(self, url, name=None):
-        f = self.opener.open(self.base_url + url)
+    def get(self, path, name=None):
+        """
+        Make an HTTP GET request.
+        
+        Arguments:
+        
+        * *path* is the relative path to request.
+        """
+        f = self.opener.open(self.base_url + path)
         data = f.read()
         f.close()
         return data
     
     @log_request
-    def post(self, url, data, name=None):
-        f = self.opener.open(self.base_url + url, urllib.urlencode(data))
+    def post(self, path, data, name=None):
+        """
+        Make an HTTP POST request.
+        
+        Arguments:
+        
+        * *path* is the relative path to request.
+        * *data* dict with the data that will be sent in the body of the POST request
+        
+        Example::
+        
+            client = HttpBrowser("http://example.com")
+            client.post("/post", {"user":"joe_hill"})
+        """
+        f = self.opener.open(self.base_url + path, urllib.urlencode(data))
         data = f.read()
         f.close()
         return data
