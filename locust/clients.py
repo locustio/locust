6a6ae9b5113b04e06286e1191120637c3f3385e8
Jonatan Heyman <jonatan@heyman.info>
2011-03-28 18:35:41 +0200

    Added support for gzip decoding in the HTTP client. Can be turned on with the the --gzip command line argument.

--- locust/clients.py
+++ locust/clients.py
@@ -4,6 +4,9 @@ from stats import log_request
 import base64
 from urlparse import urlparse, urlunparse
 
+from StringIO import StringIO
+import gzip
+
 class HTTPClient(object):
     def __init__(self, base_url):
         self.base_url = base_url
@@ -37,12 +40,14 @@ class HttpResponse(object):
     data = None
     """Response data"""
     
-    def __init__(self, url, name, code, data, info):
+    def __init__(self, url, name, code, data, info, gzip):
         self.url = url
         self._name = name
         self.code = code
         self.data = data
         self._info = info
+        self._gzip = gzip
+        self._decoded = False
     
     @property
     def info(self):
@@ -50,6 +55,17 @@ class HttpResponse(object):
         urllib2 info object containing info about the response
         """
         return self._info()
+    
+    def _get_data(self):
+        if self._gzip and not self._decoded and self._info().get("Content-Encoding") == "gzip":
+            self._data = gzip.GzipFile(fileobj=StringIO(self._data)).read()
+            self._decoded = True
+        return self._data
+    
+    def _set_data(self, data):
+        self._data = data
+    
+    data = property(_get_data, _set_data)
 
 class HttpBrowser(object):
     """
@@ -59,8 +75,9 @@ class HttpBrowser(object):
     Logs each request so that locust can display statistics.
     """
 
-    def __init__(self, base_url):
+    def __init__(self, base_url, gzip=False):
         self.base_url = base_url
+        self.gzip = gzip
         handlers = [urllib2.HTTPCookieProcessor()]
 
         # Check for basic authentication
@@ -89,18 +106,22 @@ class HttpBrowser(object):
         
         * *path* is the relative path to request.
         * *headers* is an optional dict with HTTP request headers
+        * *name* is an optional argument that can be specified to use as label in the statistics instead of the path
         
         Returns an HttpResponse instance, or None if the request failed.
         """
+        if self.gzip:
+            headers["Accept-Encoding"] = "gzip"
+        
         url = self.base_url + path
         request = urllib2.Request(url, None, headers)
         f = self.opener.open(request)
         data = f.read()
         f.close()
-        return HttpResponse(url, name, f.code, data, f.info)
+        return HttpResponse(url, name, f.code, data, f.info, self.gzip)
     
     @log_request
-    def post(self, path, data, headers={}, with_response_data=False, name=None):
+    def post(self, path, data, headers={}, name=None):
         """
         Make an HTTP POST request.
         
@@ -109,6 +130,7 @@ class HttpBrowser(object):
         * *path* is the relative path to request.
         * *data* dict with the data that will be sent in the body of the POST request
         * *headers* is an optional dict with HTTP request headers
+        * *name* Optional is an argument that can be specified to use as label in the statistics instead of the path
         
         Returns an HttpResponse instance, or None if the request failed.
         
@@ -117,10 +139,13 @@ class HttpBrowser(object):
             client = HttpBrowser("http://example.com")
             response = client.post("/post", {"user":"joe_hill"})
         """
+        if self.gzip:
+            headers["Accept-Encoding"] = "gzip"
+        
         url = self.base_url + path
         request = urllib2.Request(url, urllib.urlencode(data), headers)
         f = self.opener.open(request)
         data = f.read()
         f.close()
-        return HttpResponse(url, name, f.code, data, f.info)
+        return HttpResponse(url, name, f.code, data, f.info, self.gzip)
     
