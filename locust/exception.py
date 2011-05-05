460c57247cdd68a544485e7dc5ac910858e08cdc
Jonatan Heyman <jonatan@heyman.info>
2011-05-05 18:01:57 +0200

    Added support for making a parent locust immediately reschedule a new task when a SubLocust interrupts. Made this the default behaviour for SubLocust.interrupt().

--- locust/exception.py
+++ locust/exception.py
@@ -5,3 +5,16 @@ class InterruptLocust(Exception):
     """
     Exception that will interrupt a Locust when thrown inside a task
     """
+    
+    def __init__(self, reschedule=True):
+        """
+        If *reschedule* is True and the InterruptLocust is raised inside a SubLocust,
+        the parent Locust whould immediately reschedule another task.
+        """
+        self.reschedule = reschedule
+
+class RescheduleTaskImmediately(Exception):
+    """
+    When raised in a Locust task, another locust task will be rescheduled immediately
+    """
+    
