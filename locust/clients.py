24051b0895a1d9fbc895db131a088be8db5e10b4
Markus Thurlin <markus@esn.me>
2011-07-05 19:54:53 +0200

    Better error message from HTTP errors Patch provided by Markus Kristo

--- locust/clients.py
+++ locust/clients.py
@@ -41,12 +41,23 @@ def log_request(f):
             else:
                 events.request_success.fire(name, response_time, retval)
             return retval
-        except HTTPError, e:
+        except Exception, e:
             response_time = int((time.time() - start) * 1000)
-            events.request_failure.fire(name, response_time, e, response=e.locust_http_response)
-        except (URLError, BadStatusLine, socket.error), e:
-            response_time = int((time.time() - start) * 1000)
-            events.request_failure.fire(name, response_time, e, None)
+            extra = {}
+
+            if isinstance(e, HTTPError):
+                e.msg += " (" + name + ")"
+                extra = {"response": e.locust_http_response}
+            elif isinstance(e, URLError):
+                e.reason += " (" + name + ")"
+            elif isinstance(e, BadStatusLine):
+                e.line += " (" + name + ")"
+            elif isinstance(e, socket.error):
+                pass
+            else:
+                raise
+
+            events.request_failure.fire(name, response_time, e, **extra)
 
         if catch_response:
             return NoneContext()
