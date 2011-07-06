db43cbabf33d5452d6408c350a6f4d424bab5cbe
Markus Kristo <markus.kristo@esn.me>
2011-07-06 14:02:41 +0200

    Sorted stats by name column when running locust in terminal

--- locust/stats.py
+++ locust/stats.py
@@ -309,7 +309,8 @@ def print_stats(stats):
     total_rps = 0
     total_reqs = 0
     total_failures = 0
-    for r in stats.itervalues():
+    for key in sorted(stats.iterkeys()):
+        r = stats[key]
         total_rps += r.current_rps
         total_reqs += r.num_reqs
         total_failures += r.num_failures
@@ -329,7 +330,8 @@ def print_percentile_stats(stats):
     print (" %-" + str(STATS_NAME_WIDTH) + "s %8s %6s %6s %6s %6s %6s %6s %6s %6s %6s") % ('Name', '# reqs', '50%', '66%', '75%', '80%', '90%', '95%', '98%', '99%', '100%')
     print "-" * (80 + STATS_NAME_WIDTH)
     complete_list = []
-    for r in stats.itervalues():
+    for key in sorted(stats.iterkeys()):
+        r = stats[key]
         if r.response_times:
             print r.percentile()
             complete_list.extend(r.create_response_times_list())
