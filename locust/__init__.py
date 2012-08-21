8bb0a86f30fb7cb2513850c2da699163cbd472e1
Jonatan Heyman <jonatan@heyman.info>
2012-08-21 23:07:51 +0200

    Added RescheduleTaskImmediately to the API

--- locust/__init__.py
+++ locust/__init__.py
@@ -1,4 +1,4 @@
 from core import Locust, WebLocust, SubLocust, require_once, task
-from exception import InterruptLocust, ResponseError
+from exception import InterruptLocust, ResponseError, RescheduleTaskImmediately
 
 version = "0.5.1"
