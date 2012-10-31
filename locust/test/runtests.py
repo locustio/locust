7cea8f798f55a3b0da832224b72b90bce0b970fa
Jonatan Heyman <jonatan@heyman.info>
2012-10-31 16:25:15 +0100

    Consistent naming

--- locust/test/runtests.py
+++ locust/test/runtests.py
@@ -2,7 +2,7 @@ from gevent import monkey
 monkey.patch_all(thread=False)
 
 import unittest
-from locust_class import TestLocustClass, TestSubLocust, TestWebLocustClass, TestCatchResponse
+from test_locust_class import TestLocustClass, TestSubLocust, TestWebLocustClass, TestCatchResponse
 from test_stats import TestRequestStats, TestRequestStatsWithWebserver, TestInspectLocust
 from test_runners import TestMasterRunner
 
