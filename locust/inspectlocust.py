a2908663fe37bbc9592f3141d516b643563d0ec0
Tim Sampson <littlesanga@gmail.com>
2014-01-10 13:29:39 +0000

    simplify inspectlocust and fix "locust --show-task-ratio" with weighted locusts

--- locust/inspectlocust.py
+++ locust/inspectlocust.py
@@ -4,58 +4,46 @@ from core import Locust, TaskSet
 from log import console_logger
 
 def print_task_ratio(locusts, total=False, level=0, parent_ratio=1.0):
-    """
-    Output table with task execution ratio info to console_logger
-    """
-    ratio = {}
-    for locust in locusts:
-        ratio.setdefault(locust, 0)
-        ratio[locust] += 1
+    d = get_task_ratio_dict(locusts, total=total, parent_ratio=parent_ratio)
+    _print_task_ratio(d)
 
-    # get percentage
-    ratio_percent = dict(map(lambda x: (x[0], float(x[1])/len(locusts) * parent_ratio), ratio.iteritems()))
-    
-    for locust, ratio in ratio_percent.iteritems():
-        console_logger.info(" %-10s %-50s" % ("  "*level + "%-6.1f" % (ratio*100), "  "*level + locust.__name__))
-        if inspect.isclass(locust):
-            if issubclass(locust, Locust):
-                if total:
-                    print_task_ratio(locust.task_set.tasks, total, level+1, ratio)
-                else:
-                    print_task_ratio(locust.task_set.tasks, total, level+1)
-            elif issubclass(locust, TaskSet):
-                if total:
-                    print_task_ratio(locust.tasks, total, level+1, ratio)
-                else:
-                    print_task_ratio(locust.tasks, total, level+1)
+def _print_task_ratio(x, level=0):
+    for k, v in x.iteritems():
+        padding = 2*" "*level
+        ratio = v.get('ratio', 1)
+        console_logger.info(" %-10s %-50s" % (padding + "%-6.1f" % (ratio*100), padding + k))
+        if 'tasks' in v:
+            _print_task_ratio(v['tasks'], level + 1)
 
 
 def get_task_ratio_dict(tasks, total=False, parent_ratio=1.0):
     """
     Return a dict containing task execution ratio info
     """
+    if hasattr(tasks[0], 'weight'):
+        divisor = sum(t.weight for t in tasks)
+    else:
+        divisor = len(tasks) / parent_ratio
     ratio = {}
     for task in tasks:
         ratio.setdefault(task, 0)
-        ratio[task] += 1
+        ratio[task] += task.weight if hasattr(task, 'weight') else 1
 
     # get percentage
-    ratio_percent = dict(map(lambda x: (x[0], float(x[1])/len(tasks) * parent_ratio), ratio.iteritems()))
+    ratio_percent = dict((k, float(v) / divisor) for k, v in ratio.iteritems())
 
     task_dict = {}
     for locust, ratio in ratio_percent.iteritems():
         d = {"ratio":ratio}
         if inspect.isclass(locust):
             if issubclass(locust, Locust):
-                if total:
-                    d["tasks"] = get_task_ratio_dict(locust.task_set.tasks, total, ratio)
-                else:
-                    d["tasks"] = get_task_ratio_dict(locust.task_set.tasks, total)
+                T = locust.task_set.tasks
             elif issubclass(locust, TaskSet):
-                if total:
-                    d["tasks"] = get_task_ratio_dict(locust.tasks, total, ratio)
-                else:
-                    d["tasks"] = get_task_ratio_dict(locust.tasks, total)
+                T = locust.tasks
+            if total:
+                d["tasks"] = get_task_ratio_dict(T, total, ratio)
+            else:
+                d["tasks"] = get_task_ratio_dict(T, total)
         
         task_dict[locust.__name__] = d
 
