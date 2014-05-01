89318b90fe0782b139cef004f98d6cf9b526ff74
Jonatan Heyman <jonatan@heyman.info>
2014-05-02 01:05:10 +0200

    Use readthedocs Sphinx theme when generating docs locally

--- docs/conf.py
+++ docs/conf.py
@@ -8,6 +8,7 @@
 # All configuration values have a default value; values that are commented out
 # serve to show the default value.
 
+import os
 
 # General configuration
 # ---------------------
@@ -76,6 +77,15 @@ exclude_dirnames = []
 
 html_show_sourcelink = False
 html_file_suffix = ".html"
+
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # HTML theme
 #html_theme = "haiku"
