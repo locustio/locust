7eed3fb598995c501980a768c4a6957c14cf9ea7
Ubuntu <ubuntu@ip-10-251-42-5.ap-southeast-2.compute.internal>
2014-09-16 03:43:15 +0000

    Refactored and created environment vars that contain log file path

--- locust/main.py
+++ locust/main.py
@@ -405,9 +405,10 @@ def main():
             main_greenlet = runners.locust_runner.greenlet
     elif options.master:
         runners.locust_runner = MasterLocustRunner(locust_classes, options)
-        #DEBUG(pepe)
-        from monitor import start
-        start(runners.locust_runner)
+        from monitor import Monitor
+        monitor_log_file = os.environ["MONITOR_LOG_FILE"]
+        monitor_sleep = float(os.environ["MONITOR_SLEEP"])
+        Monitor(monitor_log_file, monitor_sleep).start(runners.locust_runner)
         
     elif options.slave:
         try:
--- locust/monitor.py
+++ locust/monitor.py
@@ -2,34 +2,52 @@ import gevent
 import sys
 import logging
 import time
+import os
 
 from subprocess import Popen
 
-logger = logging.getLogger("locust.monitor")
-handler = logging.FileHandler("/home/ubuntu/log/locust/monitor.csv")
-formatter = logging.Formatter("%(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
+class Monitor(object):
 
-SLEEP=0.5
-
-def start(master_locust=None):
-
-    logger.info("Time,Users,200,503,Current Response Time,Total Requests per Seconds")
+    def __init__(self, log_file, sleep):
+        self.log_file = log_file
+        self.sleep = sleep
         
-    while master_locust:
+        self.prepare_log_file()
+        self.prepare_logger()
 
-        if len(master_locust.stats.entries) > 0:
-            s = master_locust.stats.aggregated_stats().serialize()
-            nl = master_locust.user_count # number of users
-            nr = s['num_requests']        # number of successful requests
-            nf = s['num_failures']        # number of failed requests
-            crt = 0                       # current response time
-            if nr > 0:
-                crt = float(s['total_response_time'] / nr)
-            crps = master_locust.stats.aggregated_stats().current_rps  # current requests per/sec
+    def prepare_log_file(self):
+        d = os.path.dirname(self.log_file)
+        if not os.path.exists(d):
+            os.makedirs(d)
+        open(self.log_file, 'w').close()
 
-            logger.info("%s,%s,%s,%s,%s,%s", time.strftime("%H:%M:%S"), nl, nr, nf, crt, crps)
+    def prepare_logger(self):
+        self.logger = logging.getLogger("locust.monitor")
+        self.handler = logging.FileHandler(self.log_file)
+        self.formatter = logging.Formatter("%(message)s")
+        self.handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.handler)
+
+
+    def start(self, master_locust=None):
+
+        h = "Time,Users,Ok,Fail,RT,RPS"
+        self.logger.info(h)
         
-        gevent.sleep(SLEEP)
+        while master_locust:
+
+            if len(master_locust.stats.entries) > 0:
+                s = master_locust.stats.aggregated_stats().serialize()
+                nl = master_locust.user_count # number of users
+                nr = s['num_requests']        # number of successful requests
+                nf = s['num_failures']        # number of failed requests
+                crt = 0                       # current response time
+                if nr > 0:
+                    crt = float(s['total_response_time'] / nr)
+                crps = master_locust.stats.aggregated_stats().current_rps  # current requests per/sec
+
+                logger.info("%s,%s,%s,%s,%s,%s", time.strftime("%H:%M:%S"), nl, nr, nf, crt, crps)
+        
+            gevent.sleep(self.sleep)
+
+
