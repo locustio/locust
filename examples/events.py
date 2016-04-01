8f3a2ba8893c8725cfd93eceaccba04c091e3785
Alexey Pavlenko <alex.gripav@gmail.com>
2016-04-01 12:37:57 +0300

    Update examples/events.py with new callbacks.
    Allow collecting content-length by the master node.

--- examples/events.py
+++ examples/events.py
@@ -27,9 +27,10 @@ class WebsiteUser(HttpLocust):
 We need somewhere to store the stats.
 
 On the master node stats will contain the aggregated sum of all content-lengths,
-while on the slave nodes this will be the sum of the content-lengths since the 
+while on the slave nodes this will be the sum of the content-lengths since the
 last stats report was sent to the master
 """
+collect_content_length = False
 stats = {"content-length":0}
 
 def on_request_success(request_type, name, response_time, response_length):
@@ -44,7 +45,7 @@ def on_report_to_master(client_id, data):
     to be sent to the locust master. It will allow us to add our extra content-length
     data to the dict that is being sent, and then we clear the local stats in the slave.
     """
-    data["content-length"] = stats["content-length"]
+    data["content-length"] = stats["content-length"] if collect_content_length else 0
     stats["content-length"] = 0
 
 def on_slave_report(client_id, data):
@@ -55,10 +56,29 @@ def on_slave_report(client_id, data):
     """
     stats["content-length"] += data["content-length"]
 
+def on_configuration_to_slaves(data):
+    """
+    Event that provides an opportunity a master host to forward its configuration
+    to slaves. Here we allow collecting of content length attributes. By default
+    it is disabled by global declaration above.
+    """
+    data["collect-content-length"] = True
+
+def on_master_configuration(data):
+    """
+    Event to receive configuration sent from the master node. Here it enables collection
+    of content length attribute.
+    """
+    global collect_content_length
+    collect_content_length = data.get("collect-content-length", False)
+
+
 # Hook up the event listeners
 events.request_success += on_request_success
 events.report_to_master += on_report_to_master
 events.slave_report += on_slave_report
+events.configuration_to_slaves += on_configuration_to_slaves
+events.master_configuration += on_master_configuration
 
 @web.app.route("/content-length")
 def total_content_length():
