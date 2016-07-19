3cb7582070b41c1b498f9602805a4588f81c57cb
Stanfield Chu <schu@stanchu-ltm9.internal.salesforce.com>
2016-07-19 11:17:20 -0700

    Changes to consolidate errors

--- locust/stats.py
+++ locust/stats.py
@@ -365,9 +365,23 @@ class StatsError(object):
         self.occurences = occurences
 
     @classmethod
+    def parse_error(cls, error):
+        string_error = repr(error)
+        target = "object at 0x"
+        target_index = string_error.find(target)
+        if target_index < 0:
+            return string_error
+        start = target_index + len(target) - 2
+        end = string_error.find(">", start)
+        if end < 0:
+            return string_error
+        hex = string_error[start:end]
+        return string_error.replace(hex, "0x....")
+
+    @classmethod
     def create_key(cls, method, name, error):
-        key = "%s.%s.%r" % (method, name, error)
-        return hashlib.md5(key.encode('utf-8')).hexdigest()
+        key = "%s.%s.%r" % (method, name, StatsError.parse_error(error))
+        return hashlib.md5(key).hexdigest()
 
     def occured(self):
         self.occurences += 1
@@ -380,7 +394,7 @@ class StatsError(object):
         return {
             "method": self.method,
             "name": self.name,
-            "error": repr(self.error),
+            "error": StatsError.parse_error(self.error),
             "occurences": self.occurences
         }
 
