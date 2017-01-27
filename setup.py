b1180ba5bb05167cdb827b6d20de82bdf8080736
Mark Beacom <Beacom@gmail.com>
2017-01-27 15:58:56 -0500

    Fix #527

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
     ],
