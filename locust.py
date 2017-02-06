27f5ff9391014ba0f157fd4a5627ea0745adb63a
Mesut Güneş <mesut.gunes@morhipo.com>
2017-02-06 18:17:08 +0300

    on_start function calls only for the first hatch

---
+++ locust.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import re
+import sys
+import os
+
+
+from locust.main import main
+
+
+if __name__ == '__main__':
+	    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+	    sys.exit(main())
