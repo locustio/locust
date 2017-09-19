1e84d2fe0e566fcb45635965690dbd5eded164c5
Jonatan Heyman <jonatan@heyman.info>
2017-09-19 15:15:40 +0200

    Removed old, commented out code

--- locust/stats.py
+++ locust/stats.py
@@ -537,11 +537,9 @@ A global instance for holding the statistics. Should be removed eventually.
 """
 
 def on_request_success(request_type, name, response_time, response_length):
-    #global_stats.get(name, request_type).log(response_time, response_length)
     global_stats.log_request(request_type, name, response_time, response_length)
 
 def on_request_failure(request_type, name, response_time, exception):
-    #global_stats.get(name, request_type).log_error(exception)
     global_stats.log_error(request_type, name, exception)
 
 def on_report_to_master(client_id, data):
