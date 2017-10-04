650bf39813795047a7faf493a7e78e3a0118307d
Alexey Iefimenko <alexey.iefimenko@alexey.iefimenko MacBook>
2017-10-04 10:55:07 +0300

    Version update

--- locust/__init__.py
+++ locust/__init__.py
@@ -2,4 +2,4 @@ from .core import WebLocust, Locust, TaskSet, task, mod_context
 from .exception import InterruptTaskSet, ResponseError, RescheduleTaskImmediately
 from .config import configure, locust_config, register_config
 
-__version__ = "0.8dr1"
+__version__ = "0.8dr2"
