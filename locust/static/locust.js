b9cbed1a10cf0bc73ae53b24b12c7403e5fff1e6
rizal <rizal.rahman@tokopedia.com>
2017-10-06 17:47:55 +0700

    move the time formatter

--- locust/static/locust.js
+++ locust/static/locust.js
@@ -104,19 +104,6 @@ $('#edit_form').submit(function(event) {
     );
 });
 
-String.prototype.toHHMMSS = function () {
-    var sec_num = parseInt(this, 10);
-    var hours   = Math.floor(sec_num / 3600);
-    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    var seconds = sec_num - (hours * 3600) - (minutes * 60);
-
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    var time = hours+':'+minutes+':'+seconds;
-    return time;
-};
-
 var sortBy = function(field, reverse, primer){
     reverse = (reverse) ? -1 : 1;
     return function(a,b){
--- locust/static/timer.js
+++ locust/static/timer.js
@@ -1,6 +1,15 @@
 var sec = 0;
 var clicked = false;
 
+$(".stop-clock").click(function (event) {
+    clicked = false;
+});
+
+$(".start-clock").click(function (event) {
+    clicked = true;
+    sec = 0;
+});
+
 function updateTimer() {
     $.get('/stats/requests', function (data) {
         report = JSON.parse(data);
@@ -22,11 +31,21 @@ function updateTimer() {
 
 updateTimer();
 
-$(".stop-clock").click(function (event) {
-    clicked = false;
-});
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10);
+    var hours = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-$(".start-clock").click(function (event) {
-    clicked = true;
-    sec = 0;
-});
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    var time = hours + ':' + minutes + ':' + seconds;
+    return time;
+};
