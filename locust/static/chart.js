3a8cf12eecbab1aaca7c4828ba58e7180a314f18
rizal <rizal.rahman@tokopedia.com>
2017-10-16 17:17:38 +0700

    slider styles

--- locust/static/chart.js
+++ locust/static/chart.js
@@ -33,6 +33,7 @@
                     subtext: this.subtitle,
                     x: 10,
                     y: 10,
+                    padding: [-7,0,0,0]
                 },
                 tooltip: {
                     trigger: 'axis',
@@ -77,14 +78,44 @@
                 },
                 dataZoom: [
                     {
+                        orient: 'horizontal',
                         type: 'slider',
                         show: true,
-                        xAxisIndex: [0]
+                        height: 10,
+                        xAxisIndex: [0],
+                        bottom: 290,
+                        showDetail: false,
+                        borderColor : '#5b6f66',
+                        fillerColor: 'rgba(255, 255, 255, 0.4)',
+                        handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+                        handleSize: '175%',
+                        handleStyle: {
+                            color: '#fff',
+                            shadowBlur: 3,
+                            shadowColor: 'rgba(0, 0, 0, 0.6)',
+                            shadowOffsetX: 2,
+                            shadowOffsetY: 2
+                        }
                     },
                     {
+                        orient: 'vertical',
                         type: 'slider',
                         show: true,
-                        yAxisIndex: [0]
+                        width: 10,
+                        yAxisIndex: [0],
+                        right: 20,
+                        showDetail: false,
+                        borderColor : '#5b6f66',
+                        fillerColor: 'rgba(255, 255, 255, 0.4)',
+                        handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+                        handleSize: '175%',
+                        handleStyle: {
+                            color: '#fff',
+                            shadowBlur: 3,
+                            shadowColor: 'rgba(0, 0, 0, 0.6)',
+                            shadowOffsetX: 2,
+                            shadowOffsetY: 2
+                        }
                     },
                     {
                         type: 'inside',
