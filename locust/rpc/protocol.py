06ce8464d657de24d9692ce67db9c3f51b5b6853
Qifei Wan <qifei.wan@ele.me>
2017-10-30 13:52:38 +0800

    Support receive and handle custom stats

--- locust/rpc/protocol.py
+++ locust/rpc/protocol.py
@@ -6,10 +6,10 @@ class Message(object):
         self.type = message_type
         self.data = data
         self.node_id = node_id
-    
+
     def serialize(self):
         return msgpack.dumps((self.type, self.data, self.node_id))
-    
+
     @classmethod
     def unserialize(cls, data):
         msg = cls(*msgpack.loads(data, encoding='utf-8'))
