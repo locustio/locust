5ddff2870c5ed661998ef352aa9a8a812278482a
g.punter <g.punter@bboxx.co.uk>
2018-05-28 11:01:05 +0100

    move version to top of file to fix cannot import name version error

--- locust/__init__.py
+++ locust/__init__.py
@@ -1,6 +1,7 @@
+__version__ = "0.8.1"
+
 from .core import HttpLocust, Locust, TaskSet, task
 from .exception import InterruptTaskSet, ResponseError, RescheduleTaskImmediately
 from .parser import create_options, parse_options
 from .main import run_locust
 
-__version__ = "0.8.1"
