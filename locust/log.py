0ec8eddc23e8366b75ef3b879b6df31a620efab5
Ryan Zander <exide@hotmail.com>
2018-06-18 13:32:59 -0700

    Reverted changes to Locust's logging setup.

--- locust/log.py
+++ locust/log.py
@@ -2,26 +2,28 @@ import logging
 import socket
 import sys
 
+host = socket.gethostname()
+
 def setup_logging(loglevel, logfile):
     numeric_level = getattr(logging, loglevel.upper(), None)
     if numeric_level is None:
         raise ValueError("Invalid log level: %s" % loglevel)
     
-    log_format = "[%(asctime)s] {0}/%(levelname)s/%(name)s: %(message)s".format(socket.gethostname())
+    log_format = "[%(asctime)s] {0}/%(levelname)s/%(name)s: %(message)s".format(host)
     logging.basicConfig(level=numeric_level, filename=logfile, format=log_format)
     
     sys.stderr = StdErrWrapper()
     sys.stdout = StdOutWrapper()
 
+stdout_logger = logging.getLogger("stdout")
+stderr_logger = logging.getLogger("stderr")
+
 class StdOutWrapper(object):
     """
     Wrapper for stdout
     """
-    def __init__(self):
-        self.logger = logging.getLogger("stdout")
-
     def write(self, s):
-        self.logger.info(s.strip())
+        stdout_logger.info(s.strip())
 
     def flush(self, *args, **kwargs):
         """No-op for wrapper"""
@@ -31,11 +33,8 @@ class StdErrWrapper(object):
     """
     Wrapper for stderr
     """
-    def __init__(self):
-        self.logger = logging.getLogger("stderr")
-
     def write(self, s):
-        self.logger.error(s.strip())
+        stderr_logger.error(s.strip())
 
     def flush(self, *args, **kwargs):
         """No-op for wrapper"""
