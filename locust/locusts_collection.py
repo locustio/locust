6a05c0bbba5a4599400ed3250fb4dfc0ffdcb58b
Umqra <golovoiapklavu@gmail.com>
2018-08-05 15:03:18 +0500

    Add simple description of LocustsCollection class

--- locust/locusts_collection.py
+++ locust/locusts_collection.py
@@ -17,6 +17,14 @@ class _LocustInfo(object):
         return total_count * self.ratio - self.count
 
 class LocustsCollection(object):
+    """
+    LocustCollection maintain accurate distribution of locust classes among available locust executors according to the locust weight
+    Algorithm maintain next invariant:
+        1. Let p_i = weight_i / sum(weights)
+        2. Let count_i = p_i * locusts_count
+        3. Then, for every locust class there is at most two interger points near to count_i: floor(count_i) and ceil(count_i)
+        4. At every moment each locust class executed by floor(count_i) or ceil(count_i) locust executors
+    """
     def __init__(self, locust_classes):
         total_weight = sum(locust.weight for locust in locust_classes)
         self._locusts = [_LocustInfo(locust, locust.weight / float(total_weight)) 
