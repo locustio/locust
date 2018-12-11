197949a3084daa2e7916357f2159eda6a3b2b5a3
Jonathan McCall <jonathan.mccall@hbo.com>
2018-12-11 10:58:32 -0500

    Ensure failed zmqrpc tests clean up bound sockets

--- locust/test/test_zmqrpc.py
+++ locust/test/test_zmqrpc.py
@@ -6,30 +6,25 @@ from locust.rpc import zmqrpc, Message
 PORT = 5557
 
 class ZMQRPC_tests(unittest.TestCase):
+    def setUp(self):
+        self.server = zmqrpc.Server('*', PORT)
+        self.client = zmqrpc.Client('localhost', PORT, 'identity')
 
-    def test_server(self):
-        server = zmqrpc.Server('*', PORT)
-        server.socket.close()
-
-    def test_client(self):
-        client = zmqrpc.Client('localhost', PORT, 'identity')
-        client.socket.close()
+    def tearDown(self):
+        self.server.socket.close()
+        self.client.socket.close()
 
     def test_client_send(self):
-        server = zmqrpc.Server('*', PORT)
-        client = zmqrpc.Client('localhost', PORT, 'identity')
-        client.send(Message('test', None, None))
-        server.recv_from_client()
-        server.socket.close()
-        client.socket.close()
+        self.client.send(Message('test', None, None))
+        self.server.recv_from_client()
+        self.server.socket.close()
+        self.client.socket.close()
 
     def test_client_recv(self):
-        server = zmqrpc.Server('*', PORT)
-        client = zmqrpc.Client('localhost', PORT, 'identity')
         sleep(0.01)
         # We have to wait for the client to finish connecting 
         # before sending a msg to it.
-        server.send_to_client(Message('test', None, 'identity'))
-        client.recv()
-        server.socket.close()
-        client.socket.close()
+        self.server.send_to_client(Message('test', None, 'identity'))
+        self.client.recv()
+        self.server.socket.close()
+        self.client.socket.close()
