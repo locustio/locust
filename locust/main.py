703d5b11d5274895e5a602950da682b85b6ad237
Raiyan Kamal <raiyan.kamal@gmail.com>
2019-01-07 09:05:11 -0500

    Allow loading of modules with multiple dots in filename

--- locust/main.py
+++ locust/main.py
@@ -1,6 +1,7 @@
 import inspect
 import logging
 import os
+import imp
 import signal
 import socket
 import sys
@@ -354,8 +355,9 @@ def load_locustfile(path):
             # Add to front, then remove from original position
             sys.path.insert(0, directory)
             del sys.path[i + 1]
-    # Perform the import (trimming off the .py)
-    imported = __import__(os.path.splitext(locustfile)[0])
+    # Find the module, then perform import
+    found = imp.find_module(path)
+    imported = imp.load_module(os.path.splitext(locustfile)[0], *found)
     # Remove directory from path if we added it ourselves (just to be neat)
     if added_to_path:
         del sys.path[0]
