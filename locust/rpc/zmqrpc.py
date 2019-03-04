0be48edc8dd440a223d5ed30af2fbed58454c52f
delulu <deluzhu@yahoo.com>
2019-03-04 17:52:04 +0800

    add retry in zmqrpc

--- locust/rpc/zmqrpc.py
+++ locust/rpc/zmqrpc.py
@@ -1,24 +1,28 @@
 import zmq.green as zmq
 
 from .protocol import Message
-
+from locust.util.exception_handler import retry
 
 class BaseSocket(object):
     def __init__(self, sock_type):
         context = zmq.Context()
         self.socket = context.socket(sock_type)
-
+    
+    @retry()
     def send(self, msg):
         self.socket.send(msg.serialize())
 
+    @retry()
     def send_to_client(self, msg):
         self.socket.send_multipart([msg.node_id.encode(), msg.serialize()])
 
+    @retry()
     def recv(self):
         data = self.socket.recv()
         msg = Message.unserialize(data)
         return msg
 
+    @retry()
     def recv_from_client(self):
         data = self.socket.recv_multipart()
         addr = data[0]
