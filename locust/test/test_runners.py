80b5535c84bd2d840fb9e3c20176ef4f72e8f720
Jonatan Heyman <jonatan@heyman.info>
2019-10-24 14:43:13 +0200

    Python 2.7 fix

--- locust/test/test_runners.py
+++ locust/test/test_runners.py
@@ -112,13 +112,12 @@ class TestLocustRunner(LocustTestCase):
         self.assert_locust_class_distribution({L1:1},  runner.weight_locusts(1))
     
     def test_kill_locusts(self):
-        triggered = False
+        triggered = [False]
         class BaseLocust(Locust):
             class task_set(TaskSet):
                 @task
                 def trigger(self):
-                    nonlocal triggered
-                    triggered = True
+                    triggered[0] = True
         runner = LocustRunner([BaseLocust], mocked_options())
         runner.spawn_locusts(2, wait=False)
         self.assertEqual(2, len(runner.locusts))
@@ -128,7 +127,7 @@ class TestLocustRunner(LocustTestCase):
         self.assertEqual(0, len(runner.locusts))
         self.assertTrue(g1.dead)
         self.assertTrue(g2.dead)
-        self.assertTrue(triggered)
+        self.assertTrue(triggered[0])
         
 
 
