9631edeb27179c91427c52756897a19522417193
Jonatan Heyman <jonatan@heyman.info>
2019-10-26 16:34:40 +0200

    Added version number to deprecation warnings

--- locust/util/deprecation.py
+++ locust/util/deprecation.py
@@ -15,7 +15,7 @@ def get_class_func(f):
 def check_for_deprecated_wait_api(locust_or_taskset):
     # check if deprecated wait API is used
     if locust_or_taskset.wait_function:
-        warnings.warn("Usage of wait_function is deprecated. Declare a %s.wait_time method instead "
+        warnings.warn("Usage of wait_function is deprecated since version 0.13. Declare a %s.wait_time method instead "
                       "(should return seconds and not milliseconds)" % type(locust_or_taskset).__name__, DeprecationWarning)
         from locust.core import TaskSet
         if not locust_or_taskset.wait_time or locust_or_taskset.wait_time.__func__ == get_class_func(TaskSet.wait_time):
@@ -30,7 +30,7 @@ def check_for_deprecated_wait_api(locust_or_taskset):
                 return "%i" % int(float_value)
             else:
                 return "%.3f" % float_value
-        warnings.warn("Usage of min_wait and max_wait is deprecated. Instead use: wait_time = between(%s, %s)" % (
+        warnings.warn("Usage of min_wait and max_wait is deprecated since version 0.13. Instead use: wait_time = between(%s, %s)" % (
             format_min_max_wait(locust_or_taskset.min_wait),
             format_min_max_wait(locust_or_taskset.max_wait),
         ), DeprecationWarning)
