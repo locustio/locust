7bc9292cb7d67be0e891a3eb3f7bb2e4e89beb91
Lars Holmberg <lars.holmberg@svenskaspel.se>
2019-11-13 13:27:54 +0100

    Add response_length for failures in stats & fasthttp as well (adding to previous commit)

--- locust/stats.py
+++ locust/stats.py
@@ -610,8 +610,8 @@ A global instance for holding the statistics. Should be removed eventually.
 def on_request_success(request_type, name, response_time, response_length, **kwargs):
     global_stats.log_request(request_type, name, response_time, response_length)
 
-def on_request_failure(request_type, name, response_time, exception, **kwargs):
-    global_stats.log_request(request_type, name, response_time, 0)
+def on_request_failure(request_type, name, response_time, response_length, exception, **kwargs):
+    global_stats.log_request(request_type, name, response_time, response_length)
     global_stats.log_error(request_type, name, exception)
 
 def on_report_to_master(client_id, data):
