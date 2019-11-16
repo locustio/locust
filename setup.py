c7859a67cd9fc3d2f10e91441d268a4c24d2db0f
Jonatan Heyman <jonatan@heyman.info>
2019-11-16 02:16:50 +0100

    Pin Gevent to latest pre-release version (1.5a2). Fixes #1154.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "gevent>=1.2.2", 
+        "gevent==1.5a2", 
         "flask>=0.10.1", 
         "requests>=2.9.1", 
         "msgpack-python>=0.4.2", 
