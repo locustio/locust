4aaedfcc92d027cbab75f6de059d1a2e67c2ccc8
Lars Holmberg <lars.holmberg@svenskaspel.se>
2019-11-28 19:28:47 +0100

    Give error exit code if there were exceptions thrown. Fixes #1172

--- locust/main.py
+++ locust/main.py
@@ -527,7 +527,7 @@ def main():
         logger.info("Starting Locust %s" % version)
         main_greenlet.join()
         code = 0
-        if len(runners.locust_runner.errors):
+        if len(runners.locust_runner.errors) or len(runners.locust_runner.exceptions):
             code = options.exit_code_on_error
         shutdown(code=code)
     except KeyboardInterrupt as e:
