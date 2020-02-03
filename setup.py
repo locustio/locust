1ce72cec394a7f6048c9d5e706c2245cb88410c5
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-02-03 14:58:53 +0100

    Remove 3.5 from list of supported python versions in classifiers

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
