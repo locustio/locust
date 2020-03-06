a3149a294294b618a78ce3cf0f061381fd1dad9a
Talha Imran <timran@psu.edu>
2020-03-05 21:32:27 -0700

    Add poisson and constant_uniform wait_time functions

--- locust/__init__.py
+++ locust/__init__.py
@@ -1,5 +1,5 @@
 from .core import HttpLocust, Locust, TaskSet, TaskSequence, task, seq_task
 from .exception import InterruptTaskSet, ResponseError, RescheduleTaskImmediately
-from .wait_time import between, constant, constant_pacing
+from .wait_time import between, constant, constant_pacing, poisson, constant_uniform
 
 __version__ = "0.14.5"
--- locust/wait_time.py
+++ locust/wait_time.py
@@ -1,6 +1,8 @@
 import random
+import math
 from time import time
 
+from . import runners
 
 def between(min_wait, max_wait):
     """
@@ -56,4 +58,99 @@ def constant_pacing(wait_time):
             self._cp_last_wait_time = max(0, wait_time - run_time)
             self._cp_last_run = time()
             return self._cp_last_wait_time
+    return wait_time_func
+
+def constant_uniform(wait_time):
+    """
+    Returns a function that will track the run time of the tasks, and for each time it's 
+    called it will return a wait time that will try to make the total time between task 
+    execution equal to the time specified by the wait_time argument. The time between 
+    tasks will be maintined.
+
+    Statistically, a constant inter-arrival time between tasks will be maintained
+    regardless of number of threads and slaves, given that task run time is less than
+    specified wait_time.
+    
+    In the following example the task will always be executed once every second, no matter 
+    the task execution time. All the users will be synchronized to spread out the exact
+    execution trigger over the entire second::
+    
+        class User(Locust):
+            wait_time = constant_uniform(1)
+            class task_set(TaskSet):
+                @task
+                def my_task(self):
+                    time.sleep(random.random())
+    
+    If a task execution exceeds the specified wait_time, the wait will be 0 before starting 
+    the next task.
+    """
+    def wait_time_func(self):
+
+        locust_id = self.id
+        n_locusts = runners.locust_runner.user_count
+        locust_offset = wait_time / n_locusts * locust_id
+
+        slave_offset = 0
+        if (type(runners.locust_runner) == runners.SlaveLocustRunner):
+            slave_offset = runners.locust_runner.timeslot_ratio * wait_time / n_locusts
+            slave_offset += runners.locust_runner.clock_offset
+
+        wall_clock = time() + slave_offset + locust_offset 
+        since_last_trigger = wall_clock % wait_time
+
+        time_remaining = max(0, wait_time - since_last_trigger)
+        return time_remaining
+    return wait_time_func
+
+def poisson(lambda_value):
+    """
+    Returns a function that will track the run time of the tasks, and for each time it's 
+    called it will return a wait time that will try to achieve a poisson distribution of
+    tasks executions with lambda specified as lambda_value, given that task run time is
+    less than specified lambda_value.
+    
+    Statistically, Locust will try to ensure:
+        RPS                     = 1/lambda
+        Inter-Arrival Mean      = 1/lambda
+        Inter-Arrival Variance  = 1/(lambda^2)
+
+    Note: A reasonably high number of tasks must be executed to achieve these statistics.
+
+    In the following example, a poisson task distribution with lambda = 1 will be executed::
+    
+        class User(Locust):
+            wait_time = poisson(1)
+            class task_set(TaskSet):
+                @task
+                def my_task(self):
+                    time.sleep(random.random())
+    
+    """
+
+    # Notes on statistics:
+    #   Poisson Arrival Time Distribution = Exponential Inter-Arrival Distribution
+    wait_time = 1 / lambda_value
+
+    def random_exponential(lambda_value):
+        x = random.random()
+        return lambda_value * math.exp(-lambda_value * x)
+
+    def wait_time_func(self):
+        locust_id = self.id
+        n_locusts = runners.locust_runner.user_count
+        locust_offset = wait_time / n_locusts * locust_id
+
+        slave_offset = 0
+        if (type(runners.locust_runner) == runners.SlaveLocustRunner):
+            slave_offset = runners.locust_runner.timeslot_ratio * wait_time / n_locusts
+            slave_offset += runners.locust_runner.clock_offset
+
+        next_trigger_target = random_exponential(lambda_value) + lambda_value
+
+        wall_clock = time() + slave_offset + locust_offset 
+        since_last_trigger = wall_clock % wait_time
+
+        time_remaining = max(0, next_trigger_target - since_last_trigger)
+        return time_remaining
     return wait_time_func
