40c755824e12640b3866e4c7fbace8d3c12ea873
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-03-13 10:43:53 +0100

    Move locust.events definition from main() to __init__.py so that it is available when just importing locust instead of after main() has been run.

--- locust/__init__.py
+++ locust/__init__.py
@@ -1,5 +1,7 @@
 from .core import HttpLocust, Locust, TaskSet, TaskSequence, task, seq_task
 from .exception import InterruptTaskSet, ResponseError, RescheduleTaskImmediately
 from .wait_time import between, constant, constant_pacing
+from .event import Events
+events = Events()
 
 __version__ = "0.14.5"
