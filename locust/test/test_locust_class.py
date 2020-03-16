37399c5f2f9c7430986a4f21f8db115070812755
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-03-16 12:04:57 +0100

    fix test case AGAIN (I wanted it to test both when the Locust class has no task_set and when the TaskSet has no tasks)

--- locust/test/test_locust_class.py
+++ locust/test/test_locust_class.py
@@ -32,14 +32,14 @@ class TestTaskSet(LocustTestCase):
         self.assertEqual(t2_count, 2)
     
     def test_tasks_missing_gives_user_friendly_exception(self):
+        class MyTasks(TaskSet):
+            tasks = None
+            
         class User(Locust):
             wait_time = constant(0)
-            tasks = None
+            tasks = MyTasks
             _catch_exceptions = False
         
-        class MyTasks(TaskSet):
-            tasks = User
-
         l = MyTasks(User(self.environment))
         self.assertRaisesRegex(Exception, "No tasks defined.*", l.run)
         l.tasks = None
