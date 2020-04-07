94f0f3828bd56a226c7eed658be493df98fa5181
Anuj Sharma <anuj.sharma@trademe.co.nz>
2020-04-07 16:08:30 +1200

    Add argument to webui group

--- locust/argument_parser.py
+++ locust/argument_parser.py
@@ -173,6 +173,13 @@ def setup_parser_arguments(parser):
         action='store_true',
         help="Disable the web interface, and instead start the load test immediately. Requires -c and -t to be specified."
     )
+    web_ui_group.add_argument(
+        '--web-auth',
+        type=str,
+        dest='web_auth',
+        default=None,
+        help='Set the Basic Auth for the UI (fmt: username:password)'
+    )
     
     master_group = parser.add_argument_group(
         "Master options", 
@@ -356,13 +363,6 @@ def setup_parser_arguments(parser):
         nargs='*',
         metavar='LocustClass',
         help="Optionally specify which Locust classes that should be used (available Locust classes can be listed with -l or --list)",
-    )
-    parser.add_argument(
-        '--web-auth',
-        type=str,
-        dest='web_auth',
-        default=None,
-        help='Set the Basic Auth for the UI (fmt: username:password)'
     )
 
 def get_parser(default_config_files=DEFAULT_CONFIG_FILES):
