35514dcb70ed5ede39299802e82fa352188f3546
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-05-19 14:31:29 +0200

    Use @task decorator in taskset example

--- examples/nested_inline_tasksets.py
+++ examples/nested_inline_tasksets.py
@@ -7,7 +7,8 @@ class WebsiteUser(HttpUser):
     """
     host = "http://127.0.0.1:8089"
     wait_time = between(2, 5)
-    
+
+    @task
     class TopLevelTaskSet(TaskSet):
         @task
         class IndexTaskSet(TaskSet):
@@ -22,5 +23,3 @@ class WebsiteUser(HttpUser):
         @task
         def stats(self):
             self.client.get("/stats/requests")
-
-    tasks = [TopLevelTaskSet]
