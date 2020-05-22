1dd02be971a43b5ba5d8931ef7523032965a348f
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-05-22 08:55:56 +0200

    Bump geventhttpclient and switch back to use its original repo instead of fork, now that there is a new release (I can trigger new releases later on as well). Relates to #1116

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "requests>=2.9.1", 
         "msgpack>=0.6.2", 
         "pyzmq>=16.0.2", 
-        "geventhttpclient-wheels==1.3.1.dev3",
+        "geventhttpclient>=1.4.0",
         "ConfigArgParse>=1.0",
         "psutil>=5.6.7",
         "Flask-BasicAuth>=0.2.0"
