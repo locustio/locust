55a350db6aba50abc6664ded2b3dff52efcbe404
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-05-27 09:06:35 +0200

    Added example for how to stop a test on a certain condition.

---
+++ examples/stop_on_threshold.py
@@ -0,0 +1,30 @@
+# An example of how to stop locust if a threshold (in this case the fail ratio) is exceeded
+from locust import task, HttpUser, events
+from locust.runners import STATE_STOPPING, STATE_STOPPED, STATE_CLEANUP
+import time
+import gevent
+
+
+class MyUser(HttpUser):
+    @task
+    def my_task(self):
+        for _ in range(10):
+            self.client.get("http://www.google.com/")
+            time.sleep(1)
+        for _ in range(5):
+            self.client.get("http://www.google.com/error")
+            time.sleep(1)
+
+
+def checker(environment):
+    while not environment.runner.state in [STATE_STOPPING, STATE_STOPPED, STATE_CLEANUP]:
+        time.sleep(1)
+        if environment.runner.stats.total.fail_ratio > 0.2:
+            print(f"fail ratio was {environment.runner.stats.total.fail_ratio}, quitting")
+            environment.runner.quit()
+            return
+
+
+@events.init.add_listener
+def on_locust_init(environment, **_kwargs):
+    gevent.spawn(checker, environment)
