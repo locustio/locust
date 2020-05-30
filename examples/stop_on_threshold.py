a4da86bf14eb458dd72c6d9745fb381218b6d838
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-05-30 09:38:53 +0200

    Update stop on threshold-example

--- examples/stop_on_threshold.py
+++ examples/stop_on_threshold.py
@@ -1,18 +1,18 @@
 # An example of how to stop locust if a threshold (in this case the fail ratio) is exceeded
 from locust import task, HttpUser, events
-from locust.runners import STATE_STOPPING, STATE_STOPPED, STATE_CLEANUP
+from locust.runners import STATE_STOPPING, STATE_STOPPED, STATE_CLEANUP, WorkerRunner
 import time
 import gevent
 
-
 class MyUser(HttpUser):
+    host = "http://www.google.com"
     @task
     def my_task(self):
         for _ in range(10):
-            self.client.get("http://www.google.com/")
+            self.client.get("/")
             time.sleep(1)
         for _ in range(5):
-            self.client.get("http://www.google.com/error")
+            self.client.get("/error")
             time.sleep(1)
 
 
@@ -27,4 +27,5 @@ def checker(environment):
 
 @events.init.add_listener
 def on_locust_init(environment, **_kwargs):
-    gevent.spawn(checker, environment)
+    if not isinstance(environment.runner, WorkerRunner):
+        gevent.spawn(checker, environment)
