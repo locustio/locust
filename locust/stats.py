e8b052aeba9518f938fc5ae9de8f1e85a00eb0d6
Vladimir Stepanov <vladimirs@tradeix.com>
2020-07-13 23:58:17 +0300

    align percentile width to column width

--- locust/stats.py
+++ locust/stats.py
@@ -569,7 +569,7 @@ class StatsEntry(object):
         if not self.num_requests:
             raise ValueError("Can't calculate percentile on url with no successful requests")
 
-        tpl = f" %-{str(STATS_TYPE_WIDTH)}s %-{str(STATS_NAME_WIDTH)}s %8d {' '.join(['%6d'] * len(PERCENTILES_TO_REPORT))}"
+        tpl = f" %-{str(STATS_TYPE_WIDTH)}s %-{str(STATS_NAME_WIDTH)}s %8d {' '.join(['%7d'] * len(PERCENTILES_TO_REPORT))}"
 
         return tpl % ((self.method, self.name, self.num_requests)
                       + tuple([self.get_response_time_percentile(p) for p in PERCENTILES_TO_REPORT]))
