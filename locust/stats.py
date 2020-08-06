17c39913bb6d5c65f18779f342434f6e48ea6ce1
Vladimir Stepanov <vladimirs@tradeix.com>
2020-08-06 16:52:27 +0300

    Increase precision for percentile headers names

--- locust/stats.py
+++ locust/stats.py
@@ -699,7 +699,9 @@ def print_stats(stats, current=True):
 
 def print_percentile_stats(stats):
     console_logger.info("Percentage of the requests completed within given times")
-    headers = ('Type', 'Name', '# reqs') + tuple([f"{round(percentile*100, 4)}%" for percentile in PERCENTILES_TO_REPORT])
+    headers = ('Type', 'Name', '# reqs') + tuple(
+        [f"{int(percentile * 100) if (percentile * 100).is_integer() else round(100 * percentile, 6)}%"
+         for percentile in PERCENTILES_TO_REPORT])
     console_logger.info((f" %-{str(STATS_TYPE_WIDTH)}s %-{str(STATS_NAME_WIDTH)}s %8s "
                          f"{' '.join(['%7s'] * len(PERCENTILES_TO_REPORT))}") % headers)
     separator = f'{"-" * STATS_TYPE_WIDTH}|{"-" * STATS_NAME_WIDTH}|{"-" * 9}|{("-" * 7 + "|") * len(PERCENTILES_TO_REPORT)}'
