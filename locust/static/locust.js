9300d6ed2eb536d1891cb46b90e53e3fb40ecb3e
Ryan Warner <ryan.warner@verizondigitalmedia.com>
2020-09-25 16:26:39 -0600

    Genericize some UI classes and functions
    Allows for easier reuse of styling and functionality in extended UI elements

--- locust/static/locust.js
+++ locust/static/locust.js
@@ -1,5 +1,5 @@
-$(window).ready(function() {
-    if($("#user_count").length > 0) {
+$(window).ready(function () {
+    if ($("#user_count").length > 0) {
         $("#user_count").focus().select();
     }
 });
@@ -11,43 +11,47 @@ function appearStopped() {
     $(".user_count").hide();
 }
 
-$("#box_stop a.stop-button").click(function(event) {
+$("#box_stop a.stop-button").click(function (event) {
     event.preventDefault();
     $.get($(this).attr("href"));
     $("body").attr("class", "stopped");
     appearStopped()
 });
 
-$("#box_stop a.reset-button").click(function(event) {
+$("#box_stop a.reset-button").click(function (event) {
     event.preventDefault();
     $.get($(this).attr("href"));
 });
 
-$("#new_test").click(function(event) {
+$("#new_test").click(function (event) {
     event.preventDefault();
     $("#start").show();
     $("#user_count").focus().select();
 });
 
-$(".edit_test").click(function(event) {
+$(".edit_test").click(function (event) {
     event.preventDefault();
     $("#edit").show();
     $("#new_user_count").focus().select();
 });
 
-$(".close_link").click(function(event) {
+$(".close_link").click(function (event) {
     event.preventDefault();
     $(this).parent().parent().hide();
 });
 
-$("ul.tabs").tabs("div.panes > div").on("onClick", function(event) {
-    if (event.target == $(".chart-tab-link")[0]) {
-        // trigger resizing of charts
-        rpsChart.resize();
-        responseTimeChart.resize();
-        usersChart.resize();
-    }
+$("ul.tabs").tabs("div.panes > div").on("onClick", function (event) {
+    // trigger resizing of charts
+    resizeCharts();
 });
+
+var charts = []
+function resizeCharts() {
+    for (let index = 0; index < charts.length; index++) {
+        const chart = charts[index];
+        chart.resize();
+    }
+}
 
 var stats_tpl = $('#stats-template');
 var errors_tpl = $('#errors-template');
@@ -59,7 +63,7 @@ function setHostName(hostname) {
     $('#host_url').text(hostname);
 }
 
-$('#swarm_form').submit(function(event) {
+$('#swarm_form').submit(function (event) {
     event.preventDefault();
     $("body").attr("class", "spawning");
     $("#start").fadeOut();
@@ -69,7 +73,7 @@ $('#swarm_form').submit(function(event) {
     $("a.edit_test").fadeIn();
     $(".user_count").fadeIn();
     $.post($(this).attr("action"), $(this).serialize(),
-        function(response) {
+        function (response) {
             if (response.success) {
                 setHostName(response.host);
             }
@@ -77,10 +81,10 @@ $('#swarm_form').submit(function(event) {
     );
 });
 
-$('#edit_form').submit(function(event) {
+$('#edit_form').submit(function (event) {
     event.preventDefault();
     $.post($(this).attr("action"), $(this).serialize(),
-        function(response) {
+        function (response) {
             if (response.success) {
                 $("body").attr("class", "spawning");
                 $("#edit").fadeOut();
@@ -90,18 +94,18 @@ $('#edit_form').submit(function(event) {
     );
 });
 
-var sortBy = function(field, reverse, primer){
+var sortBy = function (field, reverse, primer) {
     reverse = (reverse) ? -1 : 1;
-    return function(a,b){
+    return function (a, b) {
         a = a[field];
         b = b[field];
-       if (typeof(primer) != 'undefined'){
-           a = primer(a);
-           b = primer(b);
-       }
-       if (a<b) return reverse * -1;
-       if (a>b) return reverse * 1;
-       return 0;
+        if (typeof (primer) != 'undefined') {
+            a = primer(a);
+            b = primer(b);
+        }
+        if (a < b) return reverse * -1;
+        if (a > b) return reverse * 1;
+        return 0;
     }
 }
 
@@ -127,8 +131,8 @@ function renderTable(report) {
     window.alternate = false;
     $('#errors tbody').jqoteapp(errors_tpl, (report.errors).sort(sortBy(sortAttribute, desc)));
 
-    $("#total_rps").html(Math.round(report.total_rps*100)/100);
-    $("#fail_ratio").html(Math.round(report.fail_ratio*100));
+    $("#total_rps").html(Math.round(report.total_rps * 100) / 100);
+    $("#fail_ratio").html(Math.round(report.fail_ratio * 100));
     $("#status_text").html(report.state);
     $("#userCount").html(report.user_count);
 }
@@ -144,14 +148,14 @@ function renderWorkerTable(report) {
 }
 
 
-$("#stats .stats_label").click(function(event) {
+$("#stats .stats_label").click(function (event) {
     event.preventDefault();
     sortAttribute = $(this).attr("data-sortkey");
     desc = !desc;
     renderTable(window.report);
 });
 
-$("#workers .stats_label").click(function(event) {
+$("#workers .stats_label").click(function (event) {
     event.preventDefault();
     WorkerSortAttribute = $(this).attr("data-sortkey");
     WorkerDesc = !WorkerDesc;
@@ -162,6 +166,7 @@ $("#workers .stats_label").click(function(event) {
 var rpsChart = new LocustLineChart($(".charts-container"), "Total Requests per Second", ["RPS", "Failures/s"], "reqs/s", ['#00ca5a', '#ff6d6d']);
 var responseTimeChart = new LocustLineChart($(".charts-container"), "Response Times (ms)", ["Median Response Time", "95% percentile"], "ms");
 var usersChart = new LocustLineChart($(".charts-container"), "Number of Users", ["Users"], "users");
+charts.push(rpsChart, responseTimeChart, usersChart)
 
 function updateStats() {
     $.get('./stats/requests', function (report) {
@@ -170,9 +175,9 @@ function updateStats() {
         renderTable(report);
         renderWorkerTable(report);
 
-        if (report.state !== "stopped"){
+        if (report.state !== "stopped") {
             // get total stats row
-            var total = report.stats[report.stats.length-1];
+            var total = report.stats[report.stats.length - 1];
             // update charts
             rpsChart.addValue([total.current_rps, total.current_fail_per_sec]);
             responseTimeChart.addValue([report.current_response_time_percentile_50, report.current_response_time_percentile_95]);
