579075b4ece9f9ec14e44afc271d951b8dc14d29
DennisKrone <dennis.krone90@gmail.com>
2020-10-01 16:49:47 +0200

    Update how runners resets time in LoadTestShape

--- locust/runners.py
+++ locust/runners.py
@@ -6,7 +6,7 @@ import sys
 import traceback
 import warnings
 from uuid import uuid4
-from time import time, monotonic
+from time import time
 
 import gevent
 import greenlet
@@ -352,7 +352,7 @@ class Runner(object):
         self.update_state(STATE_INIT)
         self.shape_greenlet = self.greenlet.spawn(self.shape_worker)
         self.shape_greenlet.link_exception(greenlet_exception_handler)
-        self.environment.shape_class.start_time = monotonic()
+        self.environment.shape_class.reset_time()
 
     def shape_worker(self):
         logger.info("Shape worker starting")
