bfa2084896552ec097528d4ac31a12c2e13ec417
Michael Kane Juncker <michael.juncker@enote.com>
2021-02-18 13:04:27 +0100

    Fix all-workers-missing condition - fixes #1707

--- locust/runners.py
+++ locust/runners.py
@@ -608,7 +608,7 @@ class MasterRunner(DistributedRunner):
                     logger.info("Worker %s failed to send heartbeat, setting state to missing." % str(client.id))
                     client.state = STATE_MISSING
                     client.user_count = 0
-                    if self.worker_count - len(self.clients.missing) <= 0:
+                    if self.worker_count <= 0:
                         logger.info("The last worker went missing, stopping test.")
                         self.stop()
                         self.check_stopped()
