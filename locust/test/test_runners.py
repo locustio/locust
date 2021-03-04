34e633813555d5398bba29cd4f5ca6ab44229796
Maxence Boutet <mboutet@explorance.com>
2021-03-03 19:49:04 -0500

    Increase sleep time to reduce test flakyness

--- locust/test/test_runners.py
+++ locust/test/test_runners.py
@@ -717,7 +717,7 @@ class TestMasterWorkerRunners(LocustTestCase):
 
             # Start a shape test
             master.start_shape()
-            sleep(0.2)
+            sleep(0.5)
 
             # First stage
             self.assertEqual("spawning", master.state)
