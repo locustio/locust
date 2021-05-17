ffffdb849be863d5a7b1bed8f14bd63ff29af7e2
Dennis Kroné <dennis.krone@svenskaspel.se>
2021-05-17 14:31:08 +0200

    Register stats from request_success and request_failure
    Old update was causing breaking issues.

--- locust/runners.py
+++ locust/runners.py
@@ -71,12 +71,15 @@ class Runner:
         self.target_user_count = None
 
         # set up event listeners for recording requests
-        def on_request(request_type, name, response_time, response_length, exception, context, **kwargs):
+        def on_request_success(request_type, name, response_time, response_length, **_kwargs):
             self.stats.log_request(request_type, name, response_time, response_length)
-            if exception:
-                self.stats.log_error(request_type, name, exception)
 
-        self.environment.events.request.add_listener(on_request)
+        def on_request_failure(request_type, name, response_time, response_length, exception, **_kwargs):
+            self.stats.log_request(request_type, name, response_time, response_length)
+            self.stats.log_error(request_type, name, exception)
+
+        self.environment.events.request_success.add_listener(on_request_success)
+        self.environment.events.request_failure.add_listener(on_request_failure)
         self.connection_broken = False
 
         # register listener that resets stats when spawning is complete
