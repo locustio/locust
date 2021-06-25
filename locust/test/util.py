8fd81f038e8e9049c9d65cb68d1c1cc976c0afbb
Maxence Boutet <mboutet@explorance.com>
2021-06-25 16:38:30 -0400

    Swallow warnings in `clear_all_functools_lru_cache` helper

--- locust/test/util.py
+++ locust/test/util.py
@@ -2,6 +2,7 @@ import functools
 import gc
 import os
 import socket
+import warnings
 
 from datetime import datetime, timedelta
 from cryptography import x509
@@ -64,10 +65,13 @@ def create_tls_cert(hostname):
 
 
 def clear_all_functools_lru_cache() -> None:
-    # Clear all `functools.lru_cache` to ensure that no state are persisted from one test to another.
-    # Taken from https://stackoverflow.com/a/50699209.
-    gc.collect()
-    wrappers = [a for a in gc.get_objects() if isinstance(a, functools._lru_cache_wrapper)]
-    assert len(wrappers) > 0
-    for wrapper in wrappers:
-        wrapper.cache_clear()
+    # Somehow, the code below throws unrelated DeprecationWarning related to Flask.
+    # We mute the warnings in order to not pollute the logs when running the tests.
+    with warnings.catch_warnings(record=True):
+        # Clear all `functools.lru_cache` to ensure that no state are persisted from one test to another.
+        # Taken from https://stackoverflow.com/a/50699209.
+        gc.collect()
+        wrappers = [a for a in gc.get_objects() if isinstance(a, functools._lru_cache_wrapper)]
+        assert len(wrappers) > 0
+        for wrapper in wrappers:
+            wrapper.cache_clear()
