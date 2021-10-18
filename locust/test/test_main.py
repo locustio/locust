224cd4408870801a8a9c0e1a54c591a113076651
Lars Holmberg <lars.holmberg@svenskaspel.se>
2021-10-18 11:48:11 +0200

    Add check to see that stats are printed even when running LoadShapes. Reorder tests to better explain when we got a timeout.

--- locust/test/test_main.py
+++ locust/test/test_main.py
@@ -393,10 +393,11 @@ class LocustProcessIntegrationTest(TestCase):
             proc.send_signal(signal.SIGTERM)
             _, stderr = proc.communicate()
             stderr = stderr.decode("utf-8")
+            self.assertIn("Aggregated", stderr)  # make sure stats printer is working
             self.assertIn("Shape test updating to 10 users at 1.00 spawn rate", stderr)
+            self.assertTrue(success, "got timeout and had to kill the process")
             self.assertIn("Cleaning up runner...", stderr)
             self.assertEqual(0, proc.returncode)
-            self.assertTrue(success, "got timeout and had to kill the process")
 
     def test_autostart_wo_run_time(self):
         port = get_free_tcp_port()
