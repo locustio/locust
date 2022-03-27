03b101f0d04834305ba945b2de90b8043887dab2
Dominik Miedziński <miedzinski@fastmail.com>
2022-03-27 22:33:33 +0200

    Add tests for pool injection

--- locust/test/test_users.py
+++ locust/test/test_users.py
@@ -1,6 +1,9 @@
 import unittest
 
-from locust import User
+from urllib3 import PoolManager
+
+from locust import User, HttpUser
+from locust.test.testcases import WebserverTestCase
 
 
 class TestUserClass(unittest.TestCase):
@@ -25,3 +28,42 @@ class TestUserClass(unittest.TestCase):
 
 class MyModuleScopedUser(User):
     pass
+
+
+class TestHttpUserWithWebserver(WebserverTestCase):
+    def test_shared_pool_manager(self):
+        shared_pool_manager = PoolManager(maxsize=1, block=True)
+
+        class MyUserA(HttpUser):
+            host = "http://127.0.0.1:%i" % self.port
+            pool_manager = shared_pool_manager
+
+        class MyUserB(HttpUser):
+            host = "http://127.0.0.1:%i" % self.port
+            pool_manager = shared_pool_manager
+
+        user_a = MyUserA(self.environment)
+        user_b = MyUserB(self.environment)
+
+        user_a.client.get("/ultra_fast")
+        user_b.client.get("/ultra_fast")
+        user_b.client.get("/ultra_fast")
+        user_a.client.get("/ultra_fast")
+
+        self.assertEqual(1, self.connections_count)
+        self.assertEqual(4, self.requests_count)
+
+    def test_pool_manager_per_user_instance(self):
+        class MyUser(HttpUser):
+            host = "http://127.0.0.1:%i" % self.port
+
+        user_a = MyUser(self.environment)
+        user_b = MyUser(self.environment)
+
+        user_a.client.get("/ultra_fast")
+        user_b.client.get("/ultra_fast")
+        user_b.client.get("/ultra_fast")
+        user_a.client.get("/ultra_fast")
+
+        self.assertEqual(2, self.connections_count)
+        self.assertEqual(4, self.requests_count)
