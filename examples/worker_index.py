1d19656ce760aa2ebcc29b004da68898776c7256
Lars Holmberg <lars.holmberg@svenskaspel.se>
2022-08-16 15:04:08 +0200

    Add example for how to use Runner.worker_index

---
+++ examples/worker_index.py
@@ -0,0 +1,19 @@
+# How to use worker_index to read from a pre-partitioned CSV file (mythings_0.csv, mythings_1.csv, ...)
+# so that each worker uses their own file
+from locust import events, task, User, runners
+from locust_plugins import csvreader  # install locust-plugins first
+
+
+class DemoUser(User):
+    reader: csvreader.CSVDictReader
+
+    @task
+    def t(self):
+        thing = next(self.reader)
+        print(thing)
+
+
+@events.init.add_listener
+def on_locust_init(environment, **_kwargs):
+    if not isinstance(environment.runner, runners.MasterRunner):
+        DemoUser.reader = csvreader.CSVDictReader(f"mythings_{environment.runner.worker_index}.csv")
