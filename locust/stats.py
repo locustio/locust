acd8709affc8486bf89715562b413c5773640d44
Balaji G <balaji.november@gmail.com>
2022-09-24 13:33:12 +0100

    Added milliseconds in the time
    I would like to have milli-seconds as well in the report. So I have changed the code (line number:886). Kindly review and approve the changes.

--- locust/stats.py
+++ locust/stats.py
@@ -883,7 +883,7 @@ def stats_history(runner: "Runner") -> None:
             break
         if runner.state != "stopped":
             r = {
-                "time": datetime.datetime.utcnow().strftime("%H:%M:%S"),
+                "time": datetime.datetime.utcnow().strftime("%H:%M:%S.%f")[:-3],
                 "current_rps": stats.total.current_rps or 0,
                 "current_fail_per_sec": stats.total.current_fail_per_sec or 0,
                 "response_time_percentile_95": stats.total.get_current_response_time_percentile(0.95) or 0,
