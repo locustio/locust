ad9544741a787824dccc551640cfc536dcdbcc1f
Samuel Spagl <samuelsp.music@gmail.com>
2022-10-12 17:23:18 +0200

    Delete the `CustomMessageListener` class for type consistency

--- locust/runners.py
+++ locust/runners.py
@@ -93,12 +93,6 @@ class ExceptionDict(TypedDict):
     nodes: Set[str]
 
 
-class CustomMessageListener(Protocol):
-    @abstractmethod
-    def __call__(self, environment: "Environment", msg: Message) -> None:
-        ...
-
-
 class Runner:
     """
     Orchestrates the load test by starting and stopping the users.
@@ -130,7 +124,7 @@ class Runner:
         self.target_user_classes_count: Dict[str, int] = {}
         # target_user_count is set before the ramp-up/ramp-down occurs.
         self.target_user_count: int = 0
-        self.custom_messages: Dict[str, CustomMessageListener] = {}
+        self.custom_messages: Dict[str, any] = {}
 
         self._users_dispatcher: Optional[UsersDispatcher] = None
 
@@ -432,7 +426,7 @@ class Runner:
         row["nodes"].add(node_id)
         self.exceptions[key] = row
 
-    def register_message(self, msg_type: str, listener: CustomMessageListener) -> None:
+    def register_message(self, msg_type: str, listener) -> None:
         """
         Register a listener for a custom message from another node
 
