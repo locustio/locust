b00252276bd6880ce2a7b31dbdb78efa3bc3f715
Andrew Baldwin <andrew.baldwin44@gmail.com>
2023-11-15 22:41:33 -0500

    Format memory usage on workers table

--- locust/webui/src/utils/string.ts
+++ locust/webui/src/utils/string.ts
@@ -48,3 +48,16 @@ export const toTitleCase = (string: string) =>
 
 export const queryStringToObject = (queryString: string) =>
   Object.fromEntries(new URLSearchParams(queryString).entries());
+
+export const formatBytes = (bytes: number, decimals = 2) => {
+  if (bytes === 0) return '0 Bytes';
+  if (bytes === 0) return 'N/A';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+};
