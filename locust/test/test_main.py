1362ee7949357afb707b577cb6fd88a55cfab20a
Lars Holmberg <lars.holmberg@svenskaspel.se>
2023-11-21 15:38:43 +0100

    more testcase updates

--- locust/test/test_main.py
+++ locust/test/test_main.py
@@ -74,7 +74,7 @@ class StandaloneIntegrationTests(ProcessIntegrationTest):
         ).strip()
         self.assertTrue(output.startswith("Usage: locust [options] [UserClass"))
         self.assertIn("Common options:", output)
-        self.assertIn("-f LOCUSTFILE, --locustfile LOCUSTFILE", output)
+        self.assertIn("-f <filename>, --locustfile <filename>", output)
         self.assertIn("Logging options:", output)
         self.assertIn("--skip-log-setup      Disable Locust's logging setup.", output)
 
