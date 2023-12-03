0c7bed03de8f3b3daf1f369112836e2f4aebe537
Andrew Baldwin <andrew.baldwin44@gmail.com>
2023-12-03 17:29:20 -0500

    Fix echarts warning

--- locust/webui/src/test/setup.ts
+++ locust/webui/src/test/setup.ts
@@ -19,3 +19,9 @@ vi.mock('@reduxjs/toolkit/query/react', async () => {
 
   return { ...actual, fetchBaseQuery: () => actual.fetchBaseQuery({ baseUrl: TEST_BASE_API }) };
 });
+
+vi.mock('echarts', async () => {
+  const actual = (await vi.importActual('echarts')) as { [key: string]: any };
+
+  return { ...actual, init: (...args: any[]) => actual.init(...args, { width: 100, height: 100 }) };
+});
