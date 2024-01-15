5c5d17a12365dd75be20af058771c28ba44ff4a5
fooqoo <horseshoes.buzz.17@gmail.com>
2024-01-16 08:53:59 +0900

    Add unittest for filterStructure

--- locust/webui/src/hooks/tests/useSelecteViewColumns.test.tsx
+++ locust/webui/src/hooks/tests/useSelecteViewColumns.test.tsx
@@ -3,6 +3,12 @@ import { describe, expect, test } from 'vitest';
 
 import useSelectViewColumns from 'hooks/useSelectViewColumns';
 
+const mockStructure = [
+  { title: 'Method', key: 'method' },
+  { title: 'Name', key: 'name' },
+  { title: '# Requests', key: 'numRequests' },
+];
+
 describe('useSelectViewColumns hook', () => {
   test('should initialize with default columns', () => {
     const defaultColumns = ['column1', 'column2'];
@@ -31,5 +37,28 @@ describe('useSelectViewColumns hook', () => {
     });
 
     expect(result.current.selectedColumns).toEqual(['column2']);
+  });
+
+  test('filterStructure should filter out unselected columns', () => {
+    const defaultColumns = mockStructure.map(({ key }) => key);
+
+    const { result } = renderHook(() => useSelectViewColumns(defaultColumns));
+
+    act(() => {
+      // remove column with key 'method'
+      result.current.removeColumn('method');
+    });
+
+    const filteredStructure = result.current.filterStructure(mockStructure);
+
+    // expect only columns with keys 'name' and 'numRequests' to be returned
+    expect(filteredStructure.length).toBe(2);
+    expect(filteredStructure).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ key: 'name' }),
+        expect.objectContaining({ key: 'numRequests' }),
+      ]),
+    );
+    expect(filteredStructure).not.toContainEqual(expect.objectContaining({ key: 'method' }));
   });
 });
