57c1777b90343ffd87ebd1094f598fb3311a3669
Andrew Baldwin <abald.cg.ide@aldogroup.com>
2024-02-22 12:13:44 +0100

    Add fallback render

--- locust/webui/src/index.tsx
+++ locust/webui/src/index.tsx
@@ -1,9 +1,11 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import Auth from 'auth';
 import ReactDOM from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Provider } from 'react-redux';
 
 import App from 'App';
+import FallbackRender from 'components/FallbackRender/FallbackRender';
 import { swarmTemplateArgs } from 'constants/swarm';
 import theme from 'redux/slice/theme.slice';
 import { store } from 'redux/store';
@@ -14,30 +16,34 @@ import { updateArraysAtProps } from 'utils/object';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-if (window.authArgs) {
-  const authStore = configureStore({
-    reducer: combineReducers({ theme }),
-  });
+function componentToRender() {
+  if (window.authArgs) {
+    const authStore = configureStore({
+      reducer: combineReducers({ theme }),
+    });
 
-  root.render(
-    <Provider store={authStore}>
-      <Auth />
-    </Provider>,
-  );
-} else if ((swarmTemplateArgs as IReportTemplateArgs).isReport) {
-  const reportProps = {
-    ...(swarmTemplateArgs as IReportTemplateArgs),
-    charts: swarmTemplateArgs.history?.reduce(
-      (charts, { currentResponseTimePercentiles, ...history }) =>
-        updateArraysAtProps(charts, { ...currentResponseTimePercentiles, ...history }),
-      {} as ICharts,
-    ) as ICharts,
-  };
-  root.render(<Report {...reportProps} />);
-} else {
-  root.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  );
+    return (
+      <Provider store={authStore}>
+        <Auth />
+      </Provider>
+    );
+  } else if ((swarmTemplateArgs as IReportTemplateArgs).isReport) {
+    const reportProps = {
+      ...(swarmTemplateArgs as IReportTemplateArgs),
+      charts: swarmTemplateArgs.history?.reduce(
+        (charts, { currentResponseTimePercentiles, ...history }) =>
+          updateArraysAtProps(charts, { ...currentResponseTimePercentiles, ...history }),
+        {} as ICharts,
+      ) as ICharts,
+    };
+    return <Report {...reportProps} />;
+  } else {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
 }
+
+root.render(<ErrorBoundary fallbackRender={FallbackRender}>{componentToRender()}</ErrorBoundary>);
