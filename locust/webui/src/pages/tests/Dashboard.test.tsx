a01844bf6f542b53bdc0f78399c1052c24e98de3
Andrew Baldwin <abald.cg.ide@aldogroup.com>
2024-02-26 12:57:52 +0100

    Add Dashboard tests

---
+++ locust/webui/src/pages/tests/Dashboard.test.tsx
@@ -0,0 +1,36 @@
+import { waitFor } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+
+import { SWARM_STATE } from 'constants/swarm';
+import Dashboard from 'pages/Dashboard';
+import { renderWithProvider } from 'test/testUtils';
+
+describe('Dashboard', () => {
+  test('renders the layout', () => {
+    const { getByRole } = renderWithProvider(<Dashboard />);
+
+    const logo = getByRole('img');
+    const heading = getByRole('link', { name: 'Locust' });
+
+    expect(heading).toBeTruthy();
+    expect(heading.getAttribute('href')).toEqual('/');
+    expect(logo).toBeTruthy();
+    expect(logo.getAttribute('src')).toEqual('/assets/logo.png');
+  });
+
+  test('renders the swarm form by default', () => {
+    const { getByText } = renderWithProvider(<Dashboard />);
+
+    expect(getByText('Start new load test')).toBeTruthy();
+  });
+
+  test('renders the tabs when swarm state is running', async () => {
+    const { getByText } = renderWithProvider(<Dashboard />, {
+      swarm: { state: SWARM_STATE.RUNNING },
+    });
+
+    await waitFor(async () => {
+      expect(getByText('Status').nextElementSibling?.textContent).toBe(SWARM_STATE.RUNNING);
+    });
+  });
+});
