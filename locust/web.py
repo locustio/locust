locustio/locust

commit 84ada19a22b076ee52060a94561ad3f968251b44

Rewrite confusing error messages if someone accesses an HTTP Web UI using https

locust/web.py
@@ -530,7 +530,17 @@ class WebUI:
                 (self.host, self.port), self.app, log=None, keyfile=self.tls_key, certfile=self.tls_cert
             )
         else:
-            self.server = pywsgi.WSGIServer((self.host, self.port), self.app, log=None)
+
+            class RewriteFilter(logging.Filter):
+                def filter(self, record) -> bool:
+                    msg = record.msg
+                    if msg.find("gevent._socket3.socket at") and msg.find("Invalid HTTP method: '\x16\x03"):
+                        record.msg = f"A request against Locust's Web UI was made using https:// instead of http:// (underlying error was {record.msg})"
+                    return True
+
+            logger.addFilter(RewriteFilter())
+            self.server = pywsgi.WSGIServer((self.host, self.port), self.app, log=None, error_log=logger)
+
         self.server.serve_forever()
 
     def stop(self):

commit 5e3f5a7c0767a5a0b5b421db9001b0d5fab8247d

Fix incorrect error message detection for https requests introduced in 84ada19a22b076ee52060a94561ad3f968251b44

locust/web.py
@@ -534,8 +534,8 @@ class WebUI:
             class RewriteFilter(logging.Filter):
                 def filter(self, record) -> bool:
                     msg = record.msg
-                    if msg.find("gevent._socket3.socket at") and msg.find("Invalid HTTP method: '\x16\x03"):
-                        record.msg = f"A request against Locust's Web UI was made using https:// instead of http:// (underlying error was {record.msg})"
+                    if "gevent._socket3.socket at" in msg and "Invalid HTTP method: '\x16\x03" in msg:
+                        record.msg = f"An https request was made against Locust's Web UI (which was expecting http). Underlying error was: {record.msg}"
                     return True
 
             logger.addFilter(RewriteFilter())