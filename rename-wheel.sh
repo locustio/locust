fdd924b04e83071a1c784bf67e6e6983f7a6b2dd
Matthew Quinn <matthew.quinn@fanduel.com>
2024-05-29 08:52:54 +0100

    remove unused py version in wheel

--- rename-wheel.sh
+++ rename-wheel.sh
@@ -4,9 +4,6 @@
 # This script renames the wheel file to a generic, multi-arch name
 # From https://github.com/python-poetry/poetry/issues/3509#issuecomment-1483859472
 
-# Rename the wheel file to match the currently active python version
-python_ver=$(python -c 'import platform; print("".join(platform.python_version_tuple()[:2]))')
-
 wheel_file=$(find dist/ -type f -name '*.whl')
 new_wheel_file=$(echo ${wheel_file} | sed "s/-cp[0-9].*$/-py3-none-any.whl/")
 
