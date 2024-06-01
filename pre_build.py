cf87454650643e9026edc79b78b112ca02d3a7ea
Matthew Quinn <matthew.quinn@fanduel.com>
2024-06-01 12:37:37 +0100

    skip front end build

--- pre_build.py
+++ pre_build.py
@@ -4,6 +4,7 @@ import subprocess
 
 def build() -> None:
     if os.environ.get("SKIP_PRE_BUILD", "") == "true":
+        print("Skipping front end build...")
         return
     print("Building front end...")
     subprocess.run(["make", "frontend_build"])
