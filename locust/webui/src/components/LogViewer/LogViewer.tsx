7047f7aa413972ddaf137afa4be9a70d55b4820f
Andrew Baldwin <andrew.baldwin44@gmail.com>
2024-06-03 14:28:23 -0400

    Update log styling

--- locust/webui/src/components/LogViewer/LogViewer.tsx
+++ locust/webui/src/components/LogViewer/LogViewer.tsx
@@ -1,21 +1,39 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Paper, Typography } from '@mui/material';
+import { red, amber, blue } from '@mui/material/colors';
 
 import { useSelector } from 'redux/hooks';
+
+const getLogColor = (log: string) => {
+  if (log.includes('CRITICAL')) {
+    return red[900];
+  }
+  if (log.includes('ERROR')) {
+    return red[700];
+  }
+  if (log.includes('WARNING')) {
+    return amber[900];
+  }
+  if (log.includes('DEBUG')) {
+    return blue[700];
+  }
+  return 'text.primary';
+};
 
 export default function LogViewer() {
   const logs = useSelector(({ logViewer: { logs } }) => logs);
 
   return (
     <Box>
-      <Typography component='h2' variant='h4'>
+      <Typography sx={{ mb: 2 }} variant='h5'>
         Logs
       </Typography>
-
-      <ul>
+      <Paper elevation={3} sx={{ p: 2, fontFamily: 'monospace' }}>
         {logs.map((log, index) => (
-          <li key={`log-${index}`}>{log}</li>
+          <Typography color={getLogColor(log)} key={`log-${index}`} variant='body2'>
+            {log}
+          </Typography>
         ))}
-      </ul>
+      </Paper>
     </Box>
   );
 }
