f7a142ee2b2f2b315dc4989cd48c9453c7940cc4
Matthew Quinn <matthew.quinn@fanduel.com>
2024-07-10 12:32:41 +0100

    provide warning for yarn presence

--- pre_build.py
+++ pre_build.py
@@ -1,8 +1,13 @@
 import os
 import subprocess
+from shutil import which
+from sys import exit
 
 
 def build() -> None:
+    if which("yarn") is None:
+        print("Locust requires the yarn binary to be available in this shell to build the web front-end.\nSee: https://docs.locust.io/en/stable/developing-locust.html#making-changes-to-locust-s-web-ui")
+        exit(1)
     if os.environ.get("SKIP_PRE_BUILD", "") == "true":
         print("Skipping front end build...")
         return
