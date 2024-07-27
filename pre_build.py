526196455f318ac881f7becab96009196b8ee188
Lars Holmberg <lars@getlocust.com>
2024-07-27 11:10:46 +0200

    skip building frontend

--- pre_build.py
+++ pre_build.py
@@ -13,16 +13,7 @@ def build() -> None:
             "Locust requires the yarn binary to be available in this shell to build the web front-end.\nSee: https://docs.locust.io/en/stable/developing-locust.html#making-changes-to-locust-s-web-ui"
         )
         exit(1)
-    print("Building front end...")
-    try:
-        if os.name == "nt":
-            subprocess.check_output(" ".join(["yarn", "webui:install"]), shell=True)
-            subprocess.check_output(" ".join(["yarn", "webui:build"]), shell=True)
-        else:
-            subprocess.run(["yarn", "webui:install"])
-            subprocess.run(["yarn", "webui:build"])
-    except subprocess.CalledProcessError as e:
-        raise AssertionError(f"Building front end with yarn failed with:\n\n{e.stdout}") from e
+    print("skipping Building front end...")
 
 
 if __name__ == "__main__":
