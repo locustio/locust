07d7fcdb9d8b89256025a0ef7e1ed144bdc6d0bf
Matthew Quinn <matthew.quinn@fanduel.com>
2024-07-30 10:04:15 +0100

    remove pre-build specifics

--- pre_build.py
+++ pre_build.py
@@ -14,8 +14,15 @@ def build() -> None:
         )
         exit(1)
     print("Building front end...")
-    subprocess.run(["yarn", "webui:install"])
-    subprocess.run(["yarn", "webui:build"])
+    try:
+        if os.name == "nt":
+            subprocess.check_output(" ".join(["yarn", "webui:install"]), shell=True)
+            subprocess.check_output(" ".join(["yarn", "webui:build"]), shell=True)
+        else:
+            subprocess.run(["yarn", "webui:install"])
+            subprocess.run(["yarn", "webui:build"])
+    except subprocess.CalledProcessError as e:
+        raise AssertionError(f"Building front end with yarn failed with:\n\n{e.stdout}") from e
 
 
 if __name__ == "__main__":
