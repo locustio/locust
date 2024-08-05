90ab24a6b21dc975151cc73cdcf9114ef8ab5d1b
Lars Holmberg <lars@getlocust.com>
2024-08-05 16:17:19 +0200

    Dont crash in flask errorhandler if error code doesnt exist. Should improve error message in #2831

--- locust/web.py
+++ locust/web.py
@@ -151,7 +151,7 @@ class WebUI:
             error_code = getattr(error, "code", 500)
             logger.log(
                 logging.INFO if error_code <= 404 else logging.ERROR,
-                f"UI got request for {request.path}, but it resulted in a {error.code}: {error.name}",
+                f"UI got request for {request.path}, but it resulted in a {error_code}: {error.name}",
             )
             return make_response(error_message, error_code)
 
