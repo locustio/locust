34a82fa95beefaaed1c65f62454a493f802d5d59
Andrew Baldwin <andrew.baldwin44@gmail.com>
2024-08-16 22:48:43 -0400

    Update logo strokeColor

--- locust/webui/src/hooks/useCreateTheme.ts
+++ locust/webui/src/hooks/useCreateTheme.ts
@@ -1,16 +1,17 @@
 import { useMemo } from 'react';
+import { Theme } from '@mui/material/styles';
 
 import { THEME_MODE } from 'constants/theme';
 import { useSelector } from 'redux/hooks';
 import createTheme from 'styles/theme';
 
-export default function useTheme() {
+export default function useCreateTheme(extendedTheme?: Theme) {
   const isDarkMode = useSelector(({ theme: { isDarkMode } }) => isDarkMode);
 
   const theme = useMemo(
-    () => createTheme(isDarkMode ? THEME_MODE.DARK : THEME_MODE.LIGHT),
+    () => createTheme(isDarkMode ? THEME_MODE.DARK : THEME_MODE.LIGHT, extendedTheme),
     [isDarkMode],
   );
 
-  return { theme, isDarkMode };
+  return theme;
 }
