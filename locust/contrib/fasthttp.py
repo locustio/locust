1746ae4fd603760f441d4ae1bae42403fd7e3d59
Master-X <153791429+MasterKey-Pro@users.noreply.github.com>
2025-01-06 09:25:17 +0800

    Update fasthttp.py
    Reuses the logic of the request method and provides type annotations to enhance code readability and consistency.

--- locust/contrib/fasthttp.py
+++ locust/contrib/fasthttp.py
@@ -298,11 +298,11 @@ class FastHttpSession:
         """Sends a GET request"""
         return self.request("GET", url, **kwargs)
 
-    def iter_lines(self, url, **kwargs):
+    def iter_lines(self, url: str, **kwargs) -> Generator[str, None, None]:
         """Sends a iter_lines request"""
-        response = self.get(url, **kwargs)
-        response.raise_for_status()
-        
+        response = self.request("GET", url, stream=True, **kwargs)
+        response.raise_for_status()  # Check for HTTP errors
+    
         buffer = ''
         for chunk in response.iter_content(chunk_size=1024):
             buffer += chunk.decode('utf-8')
