79da7c43559424e07a8fa388d81a9086166a5334
Matthew Quinn <matthew.quinn@fanduel.com>
2025-01-25 20:23:01 +0000

    UV build with front end working

---
+++ hatch_build.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+import subprocess
+from typing import Any
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface  # type: ignore
+
+
+class BuildFrontend(BuildHookInterface):
+    def initialize(self, version: str, build_data: dict[str, Any]) -> None:
+        # Only build the front end once, in the source dist, the wheel build just copies it from there
+        if self.target_name == "sdist":
+            try:
+                subprocess.check_output("yarn install", cwd="locust/webui", shell=True, stderr=subprocess.STDOUT)
+                subprocess.check_output("yarn build", cwd="locust/webui", shell=True, stderr=subprocess.STDOUT)
+            except subprocess.CalledProcessError as e:
+                raise RuntimeError(f"'{e.cmd}' got exit code {e.returncode}: {e.output}")
+
+            return super().initialize(version, build_data)
