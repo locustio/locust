1cdcde58ea3ef3114462fac2e392ea2cabdb6532
Matthew Quinn <matthew.quinn@fanduel.com>
2025-01-29 11:55:51 +0000

    local hatch targets

--- hatch_build.py
+++ hatch_build.py
@@ -10,6 +10,7 @@ class BuildFrontend(BuildHookInterface):
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
         # Only build the front end once, in the source dist, the wheel build just copies it from there
         if self.target_name == "sdist":
+            print("Building front end...")
             try:
                 subprocess.check_output("yarn install", cwd="locust/webui", shell=True, stderr=subprocess.STDOUT)
                 subprocess.check_output("yarn build", cwd="locust/webui", shell=True, stderr=subprocess.STDOUT)
