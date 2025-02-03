76995d4afb6da94b1cc67d95cfc0d93e75296fad
Matthew Quinn <matthew.quinn@fanduel.com>
2025-02-03 21:23:03 +0000

    windows?

--- hatch_build.py
+++ hatch_build.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from typing import Any
 
@@ -8,11 +9,12 @@ class BuildFrontend(BuildHookInterface):
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
         # Only build the front end once, in the source dist, the wheel build just copies it from there
         if self.target_name == "sdist":
-            print("Building front end...")
-            try:
-                subprocess.check_output("yarn install", cwd="locust/webui", shell=True, stderr=subprocess.STDOUT)
-                subprocess.check_output("yarn build", cwd="locust/webui", shell=True, stderr=subprocess.STDOUT)
-            except subprocess.CalledProcessError as e:
-                raise RuntimeError(f"'{e.cmd}' got exit code {e.returncode}: {e.output}")
+            if not os.environ.get("SKIP_PRE_BUILD"):
+                print("Building front end...")
+                try:
+                    subprocess.check_output("yarn install", cwd="locust/webui", shell=True, stderr=subprocess.STDOUT)
+                    subprocess.check_output("yarn build", cwd="locust/webui", shell=True, stderr=subprocess.STDOUT)
+                except subprocess.CalledProcessError as e:
+                    raise RuntimeError(f"'{e.cmd}' got exit code {e.returncode}: {e.output}")
 
-            return super().initialize(version, build_data)
+                return super().initialize(version, build_data)
