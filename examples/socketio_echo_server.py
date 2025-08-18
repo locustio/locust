af578839dadc0cf6ed3d209e0bd57b0c032ad7e9
Lars Holmberg <lars.holmberg@redshirt.se>
2025-08-18 13:44:29 +0200

    Make socketio_echo_server.py use geventwebsocket for concurrency and add some sleeps for more interesting results.

--- examples/socketio_echo_server.py
+++ examples/socketio_echo_server.py
@@ -1,22 +1,33 @@
-# Used by socketio_ex.py as a mock target
+# Used by socketio_ex.py as a mock target. Requires installing gevent-websocket
+import gevent.monkey
+
+gevent.monkey.patch_all()
+import time
+
 import socketio
 from flask import Flask
+from gevent import pywsgi
+from geventwebsocket.handler import WebSocketHandler
 
 # Create a Socket.IO server
-sio = socketio.Server()
+sio = socketio.Server(async_mode="gevent")
 app = Flask(__name__)
 app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
+
+DELAY = 0.01
 
 
 # When a client connects
 @sio.event
 def connect(sid, environ):
+    time.sleep(DELAY)
     print(f"Client connected: {sid}")
 
 
 # Join a room
 @sio.event
 def join_room(sid, data):
+    time.sleep(DELAY)
     room = data.get("room")
     sio.enter_room(sid, room)
     print(f"Client {sid} joined room {room}")
@@ -28,6 +39,7 @@ def join_room(sid, data):
 # Leave a room
 @sio.event
 def leave_room(sid, data):
+    time.sleep(DELAY)
     room = data.get("room")
     sio.leave_room(sid, room)
     print(f"Client {sid} left room {room}")
@@ -36,6 +48,7 @@ def leave_room(sid, data):
 # Broadcast message to a room
 @sio.event
 def send_message(sid, data):
+    time.sleep(DELAY)
     room = data.get("room")
     msg = data.get("message")
     print(f"Sent message ({msg} to room {room}")
@@ -45,10 +58,9 @@ def send_message(sid, data):
 # When a client disconnects
 @sio.event
 def disconnect(sid):
+    time.sleep(DELAY)
     print(f"Client disconnected: {sid}")
 
 
-if __name__ == "__main__":
-    import eventlet
-
-    eventlet.wsgi.server(eventlet.listen(("localhost", 5001)), app)
+server = pywsgi.WSGIServer(("0.0.0.0", 5001), app, handler_class=WebSocketHandler)
+server.serve_forever()
