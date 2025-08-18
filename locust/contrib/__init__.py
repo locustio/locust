c7bd0ee55ec4e1f828aac39818404c99c8d12349
Lars Holmberg <lars.holmberg@redshirt.se>
2025-08-18 22:50:19 +0200

    Add comment about contrib

---
+++ locust/contrib/__init__.py
@@ -0,0 +1,5 @@
+# This directory contains modules that are experimental.
+
+# Generally they should not have any significant issues, but they may change without a major version bump for locust.
+
+# Some modules may require optional dependencies, typically installed with `pip install locust[name-of-module]`.
