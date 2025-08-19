7047247f9d9f04d6a0e2d818181f83f490331225
Lars Holmberg <lars.holmberg@redshirt.se>
2025-08-19 14:25:45 +0200

    SocketIOUser: Fix use of environment object. Remove SocketIOClient.

--- locust/contrib/socketio.py
+++ locust/contrib/socketio.py
@@ -1,29 +1,30 @@
-from locust import User, events
+from locust import User
 
 import gevent
 import socketio
 
 
-class SocketIOClient(socketio.Client):
-    def __init__(self, user, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.ws_greenlet = gevent.spawn(self.wait)
-        self.user = user
-        self.on("*", self.user.on_message)
-
-
 class SocketIOUser(User):
+    """
+    SocketIOUser wraps the `socketio.Client` to log requests.
+    See socketio_ex.py for an example of how to use it.
+    """
+
     abstract = True
-    options: dict = {}  # socketio.Client options, e.g. {"reconnection_attempts": 1, "reconnection_delay": 2}
-    client: SocketIOClient
+    options: dict = {}
+    """socketio.Client options, e.g. `{"reconnection_attempts": 1, "reconnection_delay": 2}`"""
+    client: socketio.Client
+    """The underlying socketio.Client instance. Can be useful to call directly if you want to skip logging a requests."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.client = SocketIOClient(self, **self.options)
+        self.client = socketio.Client(**self.options)
+        self.ws_greenlet = gevent.spawn(self.client.wait)
+        self.client.on("*", self.on_message)
 
     # This is the default handler for events. You can override it for custom behavior,
     # or even register separate handlers using self.client.on(event, handler)
-    def on_message(self, event, data) -> None:
+    def on_message(self, event: str, data: str) -> None:
         self.environment.events.request.fire(
             request_type="WSR",
             name=event,
@@ -35,7 +36,7 @@ class SocketIOUser(User):
         )
 
     def connect(self, *args, **kwargs):
-        with events.request.measure("WS", "connect") as _:
+        with self.environment.events.request.measure("WS", "connect") as _:
             self.client.connect(*args, **kwargs)
 
     def send(self, name=None, data=None, namespace=None) -> None:
@@ -44,7 +45,7 @@ class SocketIOUser(User):
             self.client.send(name, data, namespace)
         except Exception as e:
             exception = e
-        events.request.fire(
+        self.environment.events.request.fire(
             request_type="WSS",
             name=name,
             response_time=0,
@@ -59,7 +60,7 @@ class SocketIOUser(User):
             self.client.emit(name, data, namespace, callback)
         except Exception as e:
             exception = e
-        events.request.fire(
+        self.environment.events.request.fire(
             request_type="WSE",
             name=name,
             response_time=0,
@@ -69,7 +70,7 @@ class SocketIOUser(User):
         )
 
     def call(self, event, data, *args, **kwargs):
-        with events.request.measure("WSC", event) as _:
+        with self.environment.events.request.measure("WSC", event) as _:
             return self.client.call(event, data, *args, **kwargs)
 
     def on_stop(self):
