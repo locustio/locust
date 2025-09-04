e5feb3bd06121225b601e13c7dbe691958e40d69
Lars Holmberg <lars.holmberg@redshirt.se>
2025-09-04 19:59:19 +0200

    pytest example: clean up comments

--- examples/test_pytest.py
+++ examples/test_pytest.py
@@ -3,23 +3,25 @@ from locust.contrib.fasthttp import FastHttpSession
 import time
 
 
-# pytest will automatically discover any functions prefixed with "test_" as test cases
-# session and fastsession are pytest fixtures provided by Locust's pytest plugin
+# pytest/locust will discover any functions prefixed with "test_" as test cases.
+# session and fastsession are pytest fixtures provided by Locust's pytest plugin.
 def test_stuff(fastsession: FastHttpSession):
     resp = fastsession.get("https://locust.cloud/")
 
-    # In Locust, standard request exception are ignored (and the function restarted)
-    # In pytest it fails the test
+    # In Locust's PytestUser, standard request exception are caught (and the test case restarted)
+    # In pytest any exceptions fail the test case
     resp.raise_for_status()
 
     # catch_response works just like in regular locustfiles
     with fastsession.get("https://locust.cloud/", catch_response=True) as resp:
         if not resp.text or not "success" in resp.text:
-            resp.failure("missing text in response")
+            resp.failure("important text was missing in response")
 
-    helper_function(fastsession)  # you can call helper functions as needed
+    # you can call helper functions as needed
+    helper_function(fastsession)
 
-    if fastsession.base_url:  # use --host/-H for relative paths (works for both locust and pytest)
+    # use --host/-H for relative paths (works for both locust and pytest runs)
+    if fastsession.base_url:
         fastsession.get("/")
 
     # unlike regular Locust Users, there's no wait_time, so use time.sleep instead
