d6f17cc30fd15d5310e16e90bde65ad3573b59ff
Lars Holmberg <lars.holmberg@redshirt.se>
2025-09-10 10:32:49 +0200

    Update test_pytest.py to mention raise_for_status in combination with catch_response=True

--- examples/test_pytest.py
+++ examples/test_pytest.py
@@ -8,21 +8,25 @@ import time
 def test_stuff(session: HttpSession):
     resp = session.get("https://locust.cloud/")
 
-    # In Locust's PytestUser, standard request exception are caught (and the test case restarted)
+    # In Locust's PytestUser, most Request exceptions are caught (and the test case restarted)
     # In pytest any exceptions fail the test case
     resp.raise_for_status()
 
+    # you can use --host/-H in pytest, just like with Locust, or you can set a default like this
+    if not session.base_url:
+        session.base_url = "https://locust.cloud"
+
     # catch_response works just like in regular locustfiles
-    with session.get("https://locust.cloud/", catch_response=True) as resp:
-        if not resp.text or not "success" in resp.text:
+    with session.get("/", catch_response=True) as resp:
+        if not resp.text or not "Load" in resp.text:
             resp.failure("important text was missing in response")
+
+    # raise_for_status also respects calls to resp.failure()/.success()
+    # so this will raise an exception and fail the test case if "Load" was missing
+    resp.raise_for_status()
 
     # you can call helper functions as needed
     helper_function(session)
-
-    # use --host/-H for relative paths (works for both locust and pytest runs)
-    if session.base_url:
-        session.get("/")
 
     # unlike regular Locust Users, there's no wait_time, so use time.sleep instead
     time.sleep(0.1)
@@ -30,4 +34,4 @@ def test_stuff(session: HttpSession):
 
 # this is not a test case and won't be detected by pytest/locust
 def helper_function(session: HttpSession):
-    session.get("https://locust.cloud/")
+    session.get("/")
