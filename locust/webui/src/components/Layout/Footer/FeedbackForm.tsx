01c235fd75a523b770bb2b94b2bc90f11688e84c
Andrew Baldwin <andrew.baldwin44@gmail.com>
2025-09-25 10:05:49 +0200

    Incorrect feedback api

--- locust/webui/src/components/Layout/Footer/FeedbackForm.tsx
+++ locust/webui/src/components/Layout/Footer/FeedbackForm.tsx
@@ -23,7 +23,7 @@ export default function FeedbackForm() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8000/1/customer/send-feedback', {
+      const response = await fetch('https://api.locust.cloud/1/customer/send-feedback', {
         method: 'POST',
         body: JSON.stringify(inputData),
         headers: { 'Content-Type': 'application/json' },
