36d5d50785e33a8b9319ae3235e9c1bf9440ae5f
Lars Holmberg <lars.holmberg@redshirt.se>
2025-10-17 11:47:28 +0200

    Improve example

--- examples/dns_ex.py
+++ examples/dns_ex.py
@@ -1,6 +1,8 @@
 from locust import run_single_user, task
 from locust.contrib.dns import DNSUser
 
+import time
+
 import dns.message
 import dns.rdatatype
 
@@ -9,8 +11,16 @@ class MyDNSUser(DNSUser):
     @task
     def t(self):
         message = dns.message.make_query("example.com", dns.rdatatype.A)
+        # self.client wraps all dns.query methods https://dnspython.readthedocs.io/en/stable/query.html
         self.client.udp(message, "8.8.8.8")
-        self.client.udp(message, "4.4.4.4", name="Use other dns server")
+        self.client.tcp(message, "1.1.1.1")
+        self.client.udp(
+            dns.message.make_query("doesnot-exist-1234234.com", dns.rdatatype.A),
+            "1.1.1.1",
+            name="You can rename requests",
+        )
+        # don't spam other people's DNS servers
+        time.sleep(10)
 
 
 if __name__ == "__main__":
