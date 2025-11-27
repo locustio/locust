b97089f14d49aaa030e414dc07b15ee57dbf0835
Amadeu Pereira <amadeu@locust.cloud>
2025-11-27 12:38:25 +0000

    Add test to validate name argument propagation to traces

--- locust/test/test_telemetry.py
+++ locust/test/test_telemetry.py
@@ -1,3 +1,4 @@
+import textwrap
 import unittest
 
 from .mock_locustfile import mock_locustfile
@@ -34,3 +35,31 @@ class TelemetryTests(unittest.TestCase):
                 tp.expect("OpenTelemetry enabled")
                 tp.expect("trace_id", stream="stdout")
                 tp.expect("resource_metrics", stream="stdout")
+
+    def test_request_name(self):
+        locustfile = textwrap.dedent(
+            """
+            from locust import HttpUser, task, constant
+
+            class TestUser(HttpUser):
+                host = "http://www.locust.cloud"
+                wait_time = constant(1)
+
+                @task
+                def t(self):
+                    self.client.get("/hello")
+                    self.client.get("/world", name="/custom-name")
+            """
+        )
+        with mock_locustfile(content=locustfile) as mocked:
+            with TestProcess(
+                f"locust -f {mocked.file_path} --headless -u 1 --otel --run-time 1s",
+                expect_return_code=None,
+                env={
+                    "OTEL_METRICS_EXPORTER": "none",
+                    "OTEL_TRACES_EXPORTER": "console",
+                },
+            ) as tp:
+                tp.expect("GET /hello", stream="stdout")
+                tp.expect("GET /custom-name", stream="stdout")
+                tp.not_expect_any("GET /world", stream="stdout")
