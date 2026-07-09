f32ab636351667dd80b1341e0f5920cec601a353
Lars Holmberg <lars.holmberg@redshirt.se>
2026-07-09 17:04:47 +0200

    more type-hinting

--- locust/util/rounding.py
+++ locust/util/rounding.py
@@ -1,3 +1,3 @@
-def proper_round(val, digits=0) -> int | float:
+def proper_round(val: int | float, digits=0) -> int | float:
     float_val = float(val)
     return round(float_val + 10 ** (-len(str(float_val)) - 1), digits)
